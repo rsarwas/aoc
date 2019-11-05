c4a565912db4e806822c61812165d3dccf7f87fa
Regan Sarwas <rsarwas@gmail.com>
2019-11-04 17:40:42 -0900

    Problem #2 2015 (python)

---
+++ 2015-02/answers.py
@@ -0,0 +1,32 @@
+import sys
+
+def surface_area(h, w, l):
+	return 2*h*w + 2*h*l + 2*w*l
+
+def smallest_side_area(h, w, l):
+	return min(h*w, h*l, w*l)
+
+def smallest_side_perimeter(h, w, l):
+	return min(2*h+2*w, 2*h+2*l, 2*w+2*l)
+
+def volume(h,w,l):
+	return h*w*l
+
+def ribbon(h,w,l):
+	return volume(h,w,l) + smallest_side_perimeter(h,w,l)
+
+def paper(h,w,l):
+	return surface_area(h,w,l) + smallest_side_area(h,w,l)
+
+def totalizer(packages, material):
+	total = 0	
+	for present in packages.split('\n'):
+		if 'x' in present:
+			dims = [int(dim) for dim in present.split('x')]
+			total += material(*dims)
+	return total
+
+if __name__ == '__main__':
+   input = sys.stdin.read()
+   print("Part 1: {0}".format(totalizer(input, paper)))
+   print("Part 2: {0}".format(totalizer(input, ribbon)))
