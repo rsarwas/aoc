0fce0546ec0b8665f9d2e909522a9b26844dddaa
Regan Sarwas <rsarwas@gmail.com>
2019-11-05 18:18:39 -0900

    Problem #1 2015 in C

---
+++ 2015-01/answers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int floor = 0;
+    int basement = -1;
+    int position = 0;
+    int c;
+    while((c = getchar()) != EOF) {
+        floor +=  (c == '(') ? 1 : (c == ')') ? -1: 0;
+        position++;
+        if (floor == -1 && basement == -1)
+            basement = position;
+    }
+    printf("Part 1: %d\n", floor);
+    printf("Part 2: %d\n", basement);
+   return 0;
+}
