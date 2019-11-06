5dbf0f1408d249a95e477f3f061b1d07131df80e
Regan Sarwas <rsarwas@gmail.com>
2019-11-05 18:19:28 -0900

    Problem #1 2015 Go

---
+++ 2015-01/answers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func what_floor(text string) int {
+	floor := 0
+	for i := 0; i < len(text); i++ {
+		if text[i] == '(' { floor++ }
+		if text[i] == ')' { floor-- }
+	}
+	return floor
+}
+
+func when_basement(text string) int {
+	floor := 0
+	for i := 0; i < len(text); i++ {
+		if text[i] == '(' { floor++ }
+		if text[i] == ')' { floor-- }
+		if floor == -1 { return i+1 }
+	}
+	return -1
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		fmt.Println("Part 1:", what_floor(input))
+		fmt.Println("Part 2:", when_basement(input))
+	}
+}
+
