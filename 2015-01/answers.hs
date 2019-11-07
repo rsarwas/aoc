0b36106daa14d243e1c77dd205e6db2573efe9d3
Regan Sarwas <rsarwas@gmail.com>
2019-11-06 17:29:54 -0900

    Problem #1 2015 in Haskell

---
+++ 2015-01/answers.hs
@@ -0,0 +1,15 @@
+floors :: [Char] -> [Int]
+floors xl = [if x == '(' then 1 else if x == ')' then -1 else 0 | x <- xl] 
+
+finalFloor :: [Char] -> Int
+finalFloor = sum . floors 
+
+whenBasement :: [Char] -> Int
+whenBasement xl = length $ takeWhile (>=0) $ scanl (+) 0 (floors xl)
+
+main = do
+    input <- getContents
+    putStr "Part 1: "
+    print $ finalFloor input
+    putStr "Part 2: "
+    print $ whenBasement input
