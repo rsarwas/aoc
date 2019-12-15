dc211672421c57b15f2e93892cc597bc4aa63df8
Regan Sarwas <rsarwas@gmail.com>
2019-12-14 17:43:04 -0900

    2019-04 part2 in Haskell

--- 2019-04/answers.hs
+++ 2019-04/answers.hs
@@ -2,6 +2,14 @@ cnt v w x y z
   | v == w || w == x || x == y || y == z = 10-z
   | otherwise = 1
 
+cnt' v w x y z
+    | v == w && w /= x           = 10-z
+    | v /= w && w == x && x /= y = 10-z
+    | w /= x && x == y && y /= z = 10-z
+    | x /= y && y == z           = 10-z-1
+    | y /= z                     = 1
+    | otherwise                  = 0
+
 minb x
   | x == 3    = 5
   | otherwise = x
@@ -11,3 +19,4 @@ minc x y
   | otherwise        = y
 
 t = sum [cnt a b c d e | a <- [3..7], b <- [(minb a)..9], c <- [(minc a b)..9], d <- [c..9], e <- [d..9]]
+t' = sum [cnt' a b c d e | a <- [3..7], b <- [(minb a)..9], c <- [(minc a b)..9], d <- [c..9], e <- [d..9]]
