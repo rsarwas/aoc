e22bdb53e58e0fd0c93ae9e473da3adb18999238
Regan Sarwas <rsarwas@gmail.com>
2019-12-14 15:43:00 -0900

    2019-3 part2 in Python

--- 2019-03/answers.py
+++ 2019-03/answers.py
@@ -76,7 +76,7 @@ def vertices_from_cmds(start, cmds):
 def vertices_from_string(s):
     return vertices_from_cmds((0,0), parse_cmds_from_string(s))
 
-def intersections(line1,line2):
+def intersections_md(line1,line2):
     for i in range(len(line1)-1):
         segment1 = (line1[i], line1[i+1])
         for j in range(len(line2)-1):
@@ -87,24 +87,54 @@ def intersections(line1,line2):
             if int_pt is not None and int_pt != (0,0):
                 yield man_dist(*int_pt)
 
+
+def length(segment):
+    dx = abs(segment[1][0] - segment[0][0])
+    dy = abs(segment[1][1] - segment[0][1])
+    return dx + dy
+
+
+def intersections_time(line1,line2):
+    distance1 = 0
+    for i in range(len(line1)-1):
+        distance2 = 0
+        segment1 = (line1[i], line1[i+1])
+        for j in range(len(line2)-1):
+            segment2 = (line2[j], line2[j+1])
+            # print(segment1, segment2)
+            int_pt = intersection(segment1, segment2)
+            # print(int_pt)
+            if int_pt is not None and int_pt != (0,0):
+                d1 = length((segment1[0], int_pt))
+                d2 = length((segment2[0], int_pt))
+                yield distance1 + distance2 + d1 + d2
+            distance2 += length(segment2)
+        distance1 += length(segment1)
+
 def test():
     line1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72\n"
     line1 = vertices_from_string(line1)
     line2 = "U62,R66,U55,R34,D71,R55,D58,R83\n"
     line2 = vertices_from_string(line2)
-    print(min(intersections(line1,line2)))
+    print("Part 1 - test1 {0} == 159".format(min(intersections_md(line1,line2))))
+    print("Part 2 - test1 {0} == 610".format(min(intersections_time(line1,line2))))
     line1 = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n"
     line1 = vertices_from_string(line1)
     line2 = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7\n"
     line2 = vertices_from_string(line2)
-    print(min(intersections(line1,line2)))
+    print("Part 1 - test2 {0} == 135".format(min(intersections_md(line1,line2))))
+    print("Part 2 - test2 {0} == 410".format(min(intersections_time(line1,line2))))
+
+def main():
+    lines = sys.stdin.readlines()
+    line1 = vertices_from_string(lines[0])
+    line2 = vertices_from_string(lines[1])
+    part1 = min(intersections_md(line1,line2))
+    part2 = min(intersections_time(line1,line2))
+    print("Part 1: {0}".format(part1))
+    print("Part 2: {0}".format(part2))
 
 
 if __name__ == '__main__':
     #test()
-    lines = sys.stdin.readlines()
-    line1 = vertices_from_string(lines[0])
-    line2 = vertices_from_string(lines[1])
-    part1 = min(intersections(line1,line2))
-    print("Part 1: {0}".format(part1))
-    #print("Part 2: {0}".format(part1))
+    main()
