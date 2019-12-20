09b7997550a6161130d977f23075ff4e7385243b
Regan Sarwas <rsarwas@gmail.com>
2019-12-19 19:50:43 -0900

    19-07 part2 solved with Python

--- 2019-07/answers.py
+++ 2019-07/answers.py
@@ -14,6 +14,31 @@ def max_amplification(program):
         max_thrust = max(output, max_thrust)
     return max_thrust
 
+def feedback_amplification(program):
+    max_thrust = 0
+    for phase_order in phase_setting_sequences(5,9):
+        computers = []
+        output = 0
+        for index in range(5):
+            c = Computer(program)
+            computers.append(c)
+            c.push_input(output)
+            c.push_input(phase_order[index])
+            c.start()  # do not need to check return status as it pause when it needs new input.
+            output = c.pop_output()
+        done = False
+        i = 0
+        while not done:
+            c = computers[i]
+            c.push_input(output)
+            status = c.resume()
+            output = c.pop_output()
+            done = i == 4 and status == Computer.done
+            i = (i + 1) % 5
+        max_thrust = max(output, max_thrust)
+        # print(phase_order, output)
+    return max_thrust
+
 def phase_setting_sequences(start,end):
     settings = []
     all = set(range(start,end+1))
@@ -29,3 +54,5 @@ if __name__ == '__main__':
     program = [int(x) for x in sys.stdin.read().split(',')]
     thrust = max_amplification(program)
     print("Part 1: {0}".format(thrust))
+    thrust = feedback_amplification(program)
+    print("Part 2: {0}".format(thrust))
