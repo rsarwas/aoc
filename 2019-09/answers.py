32517127d001701e98168d99780101dc5c6895cf
Regan Sarwas <rsarwas@gmail.com>
2019-12-19 21:58:24 -0900

    2019-09 in python

--- 2019-09/answers.py
+++ 2019-09/answers.py
@@ -1,18 +1,15 @@
 import sys
 from computer import Computer
 
-def test(code):
+def boost(code, input):
     c = Computer(code)
+    c.push_input(input)
     c.start()
-    out = c.pop_output()
-    diagnostic_code = out
-    while out is not None:
-        diagnostic_code = out
-        print(diagnostic_code)
-        out = c.pop_output()
-    return diagnostic_code
+    return c.pop_output()
     
 if __name__ == '__main__':
     program = [int(x) for x in sys.stdin.read().split(',')]
-    boost_keycode = test(program)
+    boost_keycode = boost(program, 1)
     print("Part 1: {0}".format(boost_keycode))
+    coords = boost(program, 2)
+    print("Part 2: {0}".format(coords))
