b4f5b7fd4dfc60fc37dff0b0bc578825f863101f
Regan Sarwas <rsarwas@gmail.com>
2019-12-21 17:56:28 -0900

    2019-13 part 2

--- 2019-13/answers.py
+++ 2019-13/answers.py
@@ -20,6 +20,9 @@ PADDLE_RIGHT = 1
 
 # Global State
 Score = None
+ball_x = 0
+x_dir = 0  # new x - old x; + to right, - = to left; 0 is impossible
+wait = True # wait for the ball to hit the paddle (about 4 turns)
 Screen = [EMPTY]*NLINES*NCOLS
 
 def play(game):
@@ -31,23 +34,45 @@ def play(game):
     # print_score()
     while status != Computer.DONE:
         move = get_paddle_move()
+        # print(ball_x, x_dir, move)
         c.push_input(move)
         status = c.resume()
         update_screen(c.get_and_clear_output())
         # draw_screen()
         # print_score()
+    print_score()
 
 def get_paddle_move():
+    global wait
     # Analyze screen to figure out how to move paddle
-    return PADDLE_HOME
+    # wait until the ball gets to the paddle, and then move
+    # the paddle with the ball
+    if wait:
+        if ball_x < 18:
+            return PADDLE_HOME
+        else:
+            # ball_x = 17 and stop waiting
+            wait = False
+    return x_dir  # x_dir = +1 moving to right == PADDLE_RIGHT
 
 def draw_screen():
     for line in format_screen():
         print(line)
 
 def print_score():
+    blocks = 0
+    ball = (0,0)
+    paddle = (0,0)
+    for i, pixel in enumerate(Screen):
+        if pixel == BLOCK:
+            blocks += 1
+        if pixel == BALL:
+            ball = (i % NCOLS, i // NCOLS)
+        if pixel == PADDLE:
+            paddle = (i % NCOLS, i // NCOLS)
     if Score is not None:
-        print("Score = {0}".format(Score))
+        print("Score = {0}; Blocks = {1}, Ball = {2}, PADDLE = {3}".format(Score, blocks, ball, paddle))
+
 
 def format_screen():
     list_matrix = [list(CHARS[EMPTY]*NCOLS) for _ in range(NLINES)]
@@ -60,12 +85,16 @@ def format_screen():
 
 def update_screen(data):
     global Score
-    Score = None
+    global ball_x, x_dir
+    #Score = None
     i = 0
     while i < len(data):
         x = data[i]
         y = data[i+1]
         cmd = data[i+2]
+        if cmd == BALL:
+            x_dir = x - ball_x
+            ball_x = x
         i += 3
         if x == -1 and y == 0:
             Score = cmd
