dc55a34babbd6d04f6d0e6ac9cca7a52bbc884e9
Regan Sarwas <rsarwas@gmail.com>
2019-12-27 19:21:43 -0900

    Lint the computer for 13 (test) and 15

--- 2019-13/computer.py
+++ 2019-13/computer.py
@@ -9,7 +9,7 @@ class Computer:
         self.__output = []
         self.__saved_ip = None
         self.__base = 0
-    
+
     def push_input(self, i):
         # i must be an int
         self.__input.append(i)
@@ -36,26 +36,36 @@ class Computer:
     def resume(self):
         self.__saved_ip = self.execute(self.__code, self.__saved_ip)
         return Computer.DONE if self.__saved_ip is None else Computer.PAUSED
-    
-    """
+
+
+    def execute(self, intcode, ip=0):
+        """
         Opcode 1 add parameter 1 and parameter 2 and store the result in parameter 3
         Opcode 2 multiply parameter 1 and parameter 2 and store the result in parameter 3
-        Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
-        Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
-        Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-        Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-        Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-        Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-        Opcode 9 adjusts the relative base by the value of its only parameter. The relative base increases (or decreases, if the value is negative) by the value of the parameter.
-    """
-    def execute(self, intcode=[99], ip=0):
-        #the ip is provide if this program is being "resumed" after a pause; otherwise it is a restart
-        instruction,pm1,pm2,pm3 = parse(read_value(intcode, ip))
+        Opcode 3 takes a single integer as input and saves it to the position given by its
+                 only parameter. For example, the instruction 3,50 would take an input value
+                 and store it at address 50.
+        Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would
+                 output the value at address 50.
+        Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction
+                 pointer to the value from the second parameter. Otherwise, it does nothing.
+        Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer
+                 to the value from the second parameter. Otherwise, it does nothing.
+        Opcode 7 is less than: if the first parameter is less than the second parameter, it stores
+                 1 in the position given by the third parameter. Otherwise, it stores 0.
+        Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in
+                 the position given by the third parameter. Otherwise, it stores 0.
+        Opcode 9 adjusts the relative base by the value of its only parameter. The relative base
+                 increases (or decreases, if the value is negative) by the value of the parameter.
+        """
+        # the ip is provide if this program is being "resumed" after a pause;
+        # otherwise it is a restart
+        instruction, pm1, pm2, pm3 = parse(read_value(intcode, ip))
         while instruction != 99:
             if instruction == 1:
-                ip = op3(ip,intcode,add, pm1, pm2, pm3, self.__base)
+                ip = op3(ip, intcode, add, pm1, pm2, pm3, self.__base)
             elif instruction == 2:
-                ip = op3(ip,intcode,mul, pm1, pm2, pm3, self.__base)
+                ip = op3(ip, intcode, mul, pm1, pm2, pm3, self.__base)
             elif instruction == 3:
                 ok, ip = self.io('r', ip, intcode, pm1, self.__base)
                 if not ok:
@@ -63,13 +73,13 @@ class Computer:
             elif instruction == 4:
                 ok, ip = self.io('w', ip, intcode, pm1, self.__base)
             elif instruction == 5:
-                ip = jump(ip,intcode,True, pm1, pm2, self.__base)
+                ip = jump(ip, intcode, True, pm1, pm2, self.__base)
             elif instruction == 6:
-                ip = jump(ip,intcode,False, pm1, pm2, self.__base)
+                ip = jump(ip, intcode, False, pm1, pm2, self.__base)
             elif instruction == 7:
-                ip = op3(ip,intcode,lt, pm1, pm2, pm3, self.__base)
+                ip = op3(ip, intcode, lt, pm1, pm2, pm3, self.__base)
             elif instruction == 8:
-                ip = op3(ip,intcode,eq, pm1, pm2, pm3, self.__base)
+                ip = op3(ip, intcode, eq, pm1, pm2, pm3, self.__base)
             elif instruction == 9:
                 ip, offset = base_offset(ip, intcode, pm1, self.__base)
                 self.__base += offset
@@ -77,7 +87,7 @@ class Computer:
                 print(ip, instruction, pm1, pm2, pm3)
                 raise NotImplementedError
             # print('  Get next instruction')
-            instruction,pm1,pm2,pm3 = parse(read_value(intcode, ip))
+            instruction, pm1, pm2, pm3 = parse(read_value(intcode, ip))
         return None
 
     # may return not OK (False) if there is nothing to read in the input queue
@@ -85,9 +95,9 @@ class Computer:
     # it when there is input in the queue
     # only update the ip if the IO operation succeeded,
     # if it failed, we will use the old ip to retry the command
-    def io(self, dir, ip, intcode, pm1=0, base=0):
+    def io(self, direction, ip, intcode, pm1=0, base=0):
         a1 = address(intcode, ip+1, pm1, base)
-        if dir == 'r':
+        if direction == 'r':
             if len(self.__input) == 0:
                 return False, ip
             write_value(intcode, a1, self.__input.pop())
@@ -97,19 +107,19 @@ class Computer:
         return True, ip
 
 
-def add(a,b):
+def add(a, b):
     return a+b
-    
-def mul(a,b):
+
+def mul(a, b):
     return a*b
 
-def lt(a,b):
+def lt(a, b):
     return 1 if a < b else 0
 
-def eq(a,b):
+def eq(a, b):
     return 1 if a == b else 0
 
-def base_offset(ip, intcode, pm1=0, base= 0):
+def base_offset(ip, intcode, pm1=0, base=0):
     a1 = address(intcode, ip+1, pm1, base)
     v1 = read_value(intcode, a1)
     ip += 2
@@ -131,8 +141,10 @@ def jump(ip, intcode, if_true, pm1=0, pm2=0, base=0):
     v1 = read_value(intcode, a1)
     v2 = read_value(intcode, a2)
     ip += 3
-    # if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-    # if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+    # if-true: if the first parameter is non-zero, it sets the instruction pointer to the value
+    #          from the second parameter. Otherwise, it does nothing.
+    # if-false: if the first parameter is zero, it sets the instruction pointer to the value from
+    #           the second parameter. Otherwise, it does nothing.
     if if_true:
         if v1 != 0:
             ip = v2
@@ -162,21 +174,21 @@ def address(code, ip, pm, base):
         return base + code[ip]
     raise NotImplementedError
 
-def read_value(code, a):
-    v = 0
+def read_value(code, addr):
+    val = 0
     try:
-        v = code[a]
+        val = code[addr]
     except IndexError:
-        # expand code to index (a) with 0
-        code += [0]*(1 + a - len(code))
-    #print('read', a, v)
-    return v
-    
-def write_value(code, a, v):
-    #print('write', a, v)
+        # expand code to index (addr) with 0
+        code += [0]*(1 + addr - len(code))
+    #print('read', addr, val)
+    return val
+
+def write_value(code, addr, val):
+    #print('write', addr, val)
     try:
-        code[a] = v 
+        code[addr] = val
     except IndexError:
-        # expand code to index (a) with 0
-        code += [0]*(1 + a - len(code))
-        code[a] = v
+        # expand code to index (addr) with 0
+        code += [0]*(1 + addr - len(code))
+        code[addr] = val
