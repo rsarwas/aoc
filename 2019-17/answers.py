83af0a99f26b08f2a80a90f815d2096e0f4f0b2b
Regan Sarwas <rsarwas@gmail.com>
2019-12-28 14:43:16 -0900

    2019-17 part 1 in python

--- 2019-17/answers.py
+++ 2019-17/answers.py
@@ -2,9 +2,36 @@ import sys
 from computer import Computer
 
 def solve(intcode):
+    def is_intersections(scaffold, index, platform, nchars):
+        if scaffold[index] != platform:
+            return False
+        if scaffold[index-1] != platform:
+            return False
+        if scaffold[index+1] != platform:
+            return False
+        if scaffold[index-nchars] != platform:
+            return False
+        if scaffold[index+nchars] != platform:
+            return False
+        return True
     computer = Computer(intcode)
-    status = computer.start()
-    return status
+    computer.start()
+    scaffold = computer.get_and_clear_output()
+    # scaffold_string = ''.join([chr(a) for a in scaffold])
+    # print(scaffold_string)
+    # From inspecting the printed scaffold:
+    nlines = 33
+    nchars = 52 # includes the newline (13)
+    platform = 35
+    sum_of_locations = 0
+    for line in range(1, nlines-1):
+        for char in range(1, nchars-1):
+            index = line*nchars + char
+            if scaffold[index] == platform:
+                if is_intersections(scaffold, index, platform, nchars):
+                    # print((line, char))
+                    sum_of_locations += line * char
+    return sum_of_locations
 
 def main():
     program = [int(x) for x in sys.stdin.read().split(',')]
