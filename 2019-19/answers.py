07479d870748144b9f0d9314c5475453f25e5c40
Regan Sarwas <rsarwas@gmail.com>
2019-12-28 20:00:35 -0900

    2019-19 part1 python

--- 2019-19/answers.py
+++ 2019-19/answers.py
@@ -2,9 +2,26 @@ import sys
 from computer import Computer
 
 def solve(intcode):
-    computer = Computer(intcode)
-    status = computer.start()
-    return status
+    # Simple brute force solution
+    # we could print the 10 by 10 results to see if there is a cone pattern
+    # and limit the edges of the cone, but this is just too easy.
+
+    # While not stated clearly in the instructions, the program halts after
+    # creating the output.  It cannot be resumed.  It can also not be restarted
+    # I need to "reload" the int code for each run.
+
+    total = 0
+    for x in range(50):
+        for y in range(50):
+            computer = Computer(intcode)
+            computer.push_input(y)
+            computer.push_input(x)
+            computer.start()
+            output = computer.pop_output()
+            if output == 1:
+                total += 1
+            # print(x, y, output, total)
+    return total
 
 def main():
     program = [int(x) for x in sys.stdin.read().split(',')]
