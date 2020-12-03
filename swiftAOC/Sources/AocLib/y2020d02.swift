c2949fea8ca53fdadd2ea952cf39e8d50c2ff656
Regan Sarwas <rsarwas@gmail.com>
2020-12-02 19:48:16 -0900

    Rewrite 2020-02 swift (functional)

--- swiftAOC/Sources/AocLib/y2020d02.swift
+++ swiftAOC/Sources/AocLib/y2020d02.swift
@@ -1,4 +1,5 @@
 import Foundation
+
 struct Problem202002: Problem {
   var name: String { "2020-02" }
   func solveWith(data: [String]) -> Solution { Solution202002(data: data) }
@@ -8,58 +9,56 @@ struct Solution202002: Solution {
   let data: [String]
 
   var part1: String {
-    //let answer = "Not Implemented"
-    return "\(answer1)"
+    return "\(passwordCountPassingTest1)"
   }
 
   var part2: String {
-    return "\(answer2)"
+    return "\(passwordCountPassingTest2)"
   }
 
-  var answer1: Int {
-    var validPasswordCount = 0
-    for record in data {
-      var parts = record.split(separator: ":")
-      let policy = parts[0]
-      let password = parts[1]
-      parts = policy.split(separator: " ")
-      let range = parts[0]
-      let neededChar = parts[1].first
-      parts = range.split(separator: "-")
-      let minimum = Int(parts[0]) ?? 9999
-      let maximum = Int(parts[1]) ?? -1
-      let neededChars = password.filter { $0 == neededChar }
-      password.unicodeScalars
-      if neededChars.count >= minimum && neededChars.count <= maximum {
-        validPasswordCount += 1
-      }
-    }
-    return validPasswordCount
+  var passwordCountPassingTest1: Int {
+    return data.compactMap { $0.asPasswordChecker }
+      .filter { $0.passingTest1 }
+      .count
   }
 
-  var answer2: Int {
-    var validPasswordCount = 0
-    for record in data {
-      var parts = record.split(separator: ":")
-      let policy = parts[0]
-      let password = String(parts[1]).trimmingCharacters(in: .whitespacesAndNewlines)
-      parts = policy.split(separator: " ")
-      let range = parts[0]
-      let neededChar = parts[1].first!
-      parts = range.split(separator: "-")
-      let minimum = -1 + (Int(parts[0]) ?? 9999)
-      let maximum = -1 + (Int(parts[1]) ?? -1)
-      let index1 = password.index(password.startIndex, offsetBy: minimum)
-      let index2 = password.index(password.startIndex, offsetBy: maximum)
-      //print("Policy \(policy): Search \(password) for \(neededChar), first: \(password[index1]), second: \(password[index2])")
-      var found = 0
-      if password[index1] == neededChar { found += 1 }
-      if password[index2] == neededChar { found += 1 }
-      if found == 1 {
-        validPasswordCount += 1
-      }
-    }
-    return validPasswordCount
+  var passwordCountPassingTest2: Int {
+    return data.compactMap { $0.asPasswordChecker }
+      .filter { $0.passingTest2 }
+      .count
+  }
+
+}
+
+extension String {
+  var asPasswordChecker: PasswordChecker? {
+    let parts = self.split(separator: " ")
+    guard parts.count == 3 else { return nil }
+    let rangeParts = parts[0].split(separator: "-")
+    guard rangeParts.count == 2 else { return nil }
+    guard let int1 = Int(rangeParts[0]), let int2 = Int(rangeParts[1]) else { return nil }
+    guard let char1 = parts[1].first else { return nil }
+    let password = String(parts[2])
+    return PasswordChecker(int1: int1, int2: int2, char1: char1, password: password)
+  }
+}
+
+struct PasswordChecker {
+  let int1: Int
+  let int2: Int
+  let char1: Character
+  let password: String
+
+  var passingTest1: Bool {
+    let count = password.filter { $0 == char1 }.count
+    return int1 <= count && count <= int2
+  }
+
+  var passingTest2: Bool {
+    let count = password.enumerated().filter { (n, c) in
+      c == char1 && (n + 1 == int1 || n + 1 == int2)
+    }.count
+    return count == 1
   }
 
 }
