64443607fbb0771483e696de2e60b7c39be38e99
Regan Sarwas <rsarwas@gmail.com>
2020-12-15 21:47:59 -0900

    2020day16 part1 Swift solved w/o input parser

--- swiftAOC/Sources/AocLib/y2020d16.swift
+++ swiftAOC/Sources/AocLib/y2020d16.swift
@@ -15,12 +15,69 @@ struct Solution202016: Solution {
   }
 
   var answer1: Int {
-    //let adapters = data.compactMap { Int($0) }.sorted()
-    return -1
+    guard let puzzle = Puzzle(input: data) else { return -1}
+    return puzzle.scanningErrorRate
   }
 
   var answer2: Int {
     return -1
   }
 
+}
+
+struct Puzzle {
+
+  let tickets: [Ticket]
+  let myTicketNumber = 0
+  let ranges: [String: (ClosedRange<Int>,ClosedRange<Int>)]
+
+  init?(input: [String]) {
+    ranges = [
+      "class": (1...3, 5...7),
+      "row": (6...11, 33...44),
+      "seat": (13...40, 45...50)
+    ]
+    tickets = [
+      [7,1,14],
+      [7,3,47],
+      [40,4,50],
+      [55,2,20],
+      [38,6,12]
+    ]
+  }
+
+  var scanningErrorRate: Int {
+    return invalidOtherFields.reduce(0, +)
+  }
+
+  var invalidOtherFields: [Int] {
+    let invalids = otherTickets.map { invalidFields($0) }
+    return invalids.reduce([], +) //Flatten [[Int]] -> [Int]
+  }
+
+  var myTicket: [Int] {
+    return tickets[myTicketNumber]
+  }
+
+  var otherTickets: Array<[Int]>.SubSequence {
+    tickets.dropFirst()
+  }
+
+  func invalidFields(_ ticket: Ticket) -> [Int] {
+    return ticket.filter { !valid(field: $0) }
+  }
+
+  func valid(field: Int) -> Bool {
+    for (range1, range2) in ranges.values {
+      if range1.contains(field) || range2.contains(field) { return true }
+    }
+    print(field)
+    return false
+  }
+
+}
+
+typealias Ticket = [Int]
+
+extension Ticket {
 }
--- swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
+++ swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
@@ -553,7 +553,18 @@ final class swiftAOCTests: XCTestCase {
 
   func test202016a() {
     let testCases = [
-      ([""], "-1")
+      (["class: 1-3 or 5-7",
+        "row: 6-11 or 33-44",
+        "seat: 13-40 or 45-50",
+        "",
+        "your ticket:",
+        "7,1,14",
+        "",
+        "nearby tickets:",
+        "7,3,47",
+        "40,4,50",
+        "55,2,20",
+        "38,6,12"], "71")
     ]
     for (input, answer) in testCases {
       let solution = Solution202016(data: input)
