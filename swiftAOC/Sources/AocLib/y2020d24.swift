ddad6cbac5e9814e341c295a1280abb3c0073fe6
Regan Sarwas <rsarwas@gmail.com>
2020-12-24 12:34:15 -0900

    Solve 2020 day 24 part 1 w/ swift

--- swiftAOC/Sources/AocLib/y2020d24.swift
+++ swiftAOC/Sources/AocLib/y2020d24.swift
@@ -15,8 +15,17 @@ struct Solution202024: Solution {
   }
 
   var answer1: Int {
-    //let adapters = data.compactMap { Int($0) }.sorted()
-    return -1
+    // A tile is identified by it's HexCoordinates
+    let tiles = data.compactMap { $0.asHexCoord }
+    var blackTiles = Set<HexCoord>()
+    for tile in tiles {
+      if blackTiles.contains(tile) {
+        blackTiles.remove(tile)
+      } else {
+        blackTiles.insert(tile)
+      }
+    }
+    return blackTiles.count
   }
 
   var answer2: Int {
@@ -24,3 +33,55 @@ struct Solution202024: Solution {
   }
 
 }
+
+struct HexCoord: Hashable {
+    let x: Int
+    let y: Int
+
+/*
+   X Coordinates are staggered for Hexagons; Increase to the East:
+2  0 1 2 3 4
+1   0 1 2 3 4
+0    0 1 2 3 4
+Y Coordinates Increase to the North
+*/
+  static let moves = [
+    "se": (0,-1),
+    "sw": (-1,-1),
+    "e": (1,0),
+    "w": (-1,0),
+    "ne": (1,1),
+    "nw": (0,1)
+  ]
+
+}
+
+extension  HexCoord {
+
+  func move(dir: String) -> HexCoord? {
+    guard let (dx,dy) = HexCoord.moves[dir] else { return nil }
+    return HexCoord(x: self.x + dx, y: self.y + dy)
+  }
+
+}
+extension String {
+  var asHexCoord : HexCoord? {
+    var coord = HexCoord(x:0,y:0)
+    var prefix : Character? = nil
+    for char in self {
+      switch (prefix, char) {
+      case (nil, "e"): coord = coord.move(dir: "e")!; break
+      case (nil, "w"): coord = coord.move(dir: "w")!; break
+      case (nil, "s"): prefix = "s"; break
+      case (nil, "n"): prefix = "n"; break
+      case ("s", "e"): prefix = nil; coord = coord.move(dir: "se")!; break
+      case ("s", "w"): prefix = nil; coord = coord.move(dir: "sw")!; break
+      case ("n", "e"): prefix = nil; coord = coord.move(dir: "ne")!; break
+      case ("n", "w"): prefix = nil; coord = coord.move(dir: "nw")!; break
+      default: return nil
+      }
+    }
+    return coord
+  }
+}
+
--- swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
+++ swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
@@ -760,15 +760,15 @@ final class swiftAOCTests: XCTestCase {
   //   }
   // }
 
-  func test202023a() {
-    let testCases = [
-      (["389125467"], "67384529")
-    ]
-    for (input, answer) in testCases {
-      let solution = Solution202023(data: input)
-      XCTAssertEqual(solution.part1, answer)
-    }
-  }
+  // func test202023a() {
+  //   let testCases = [
+  //     (["389125467"], "67384529")
+  //   ]
+  //   for (input, answer) in testCases {
+  //     let solution = Solution202023(data: input)
+  //     XCTAssertEqual(solution.part1, answer)
+  //   }
+  // }
 
   // func test202023b() {
   //   let testCases = [
@@ -780,15 +780,34 @@ final class swiftAOCTests: XCTestCase {
   //   }
   // }
 
-  // func test202024a() {
-  //   let testCases = [
-  //     ([""], "-1")
-  //   ]
-  //   for (input, answer) in testCases {
-  //     let solution = Solution202024(data: input)
-  //     XCTAssertEqual(solution.part1, answer)
-  //   }
-  // }
+  func test202024a() {
+    let testCases = [
+      (["sesenwnenenewseeswwswswwnenewsewsw",
+        "neeenesenwnwwswnenewnwwsewnenwseswesw",
+        "seswneswswsenwwnwse",
+        "nwnwneseeswswnenewneswwnewseswneseene",
+        "swweswneswnenwsewnwneneseenw",
+        "eesenwseswswnenwswnwnwsewwnwsene",
+        "sewnenenenesenwsewnenwwwse",
+        "wenwwweseeeweswwwnwwe",
+        "wsweesenenewnwwnwsenewsenwwsesesenwne",
+        "neeswseenwwswnwswswnw",
+        "nenwswwsewswnenenewsenwsenwnesesenew",
+        "enewnwewneswsewnwswenweswnenwsenwsw",
+        "sweneswneswneneenwnewenewwneswswnese",
+        "swwesenesewenwneswnwwneseswwne",
+        "enesenwswwswneneswsenwnewswseenwsese",
+        "wnwnesenesenenwwnenwsewesewsesesew",
+        "nenewswnwewswnenesenwnesewesw",
+        "eneswnwswnwsenenwnwnwwseeswneewsenese",
+        "neswnwewnwnwseenwseesewsenwsweewe",
+        "wseweeenwnesenwwwswnew"], "10")
+    ]
+    for (input, answer) in testCases {
+      let solution = Solution202024(data: input)
+      XCTAssertEqual(solution.part1, answer)
+    }
+  }
 
   // func test202024b() {
   //   let testCases = [
