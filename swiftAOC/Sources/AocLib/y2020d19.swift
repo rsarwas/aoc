4ea08d0be71495f7d131d2cdb205b43aa542ba66
Regan Sarwas <rsarwas@gmail.com>
2021-01-03 14:40:16 -0900

    Solve 2020 day 19 part 2

--- swiftAOC/Sources/AocLib/y2020d19.swift
+++ swiftAOC/Sources/AocLib/y2020d19.swift
@@ -25,7 +25,15 @@ struct Solution202019: Solution {
   }
 
   var answer2: Int {
-    return -1
+    let dataParts = data.split(separator: "")
+    let parser = Parser(ruleText:dataParts[0])
+    // for testing:
+    // for message in dataParts[1] {
+    //   print("message:\"\(message)\"")
+    //   print("isValid: \(parser.isValid2(message: message))")
+    // }
+    // return -1
+    return dataParts[1].filter({parser.isValid2(message: $0)}).count
   }
 
 }
@@ -33,7 +41,7 @@ struct Solution202019: Solution {
 import Foundation // for String.splitOnOccurence
 class Parser {
 
-  let rules: [RuleId:Rule]
+  var rules: [RuleId:Rule]
   var message: [Character]
 
   init(ruleText: Array<String>.SubSequence) {
@@ -55,6 +63,62 @@ class Parser {
     guard !message.isEmpty else {return false }
     self.message = message.reversed()
     return isMessageValid(rule: rule) && self.message.isEmpty
+  }
+
+  func isValid2(message: String) -> Bool {
+    // In the original problem 0 -> 8 11; 8 -> 42; 11 -> 42 31
+    // or put another way: 0 ->  42 42 31
+    // No other rules call 8, 11, 31 or 42 (in test or real input)
+    // With the new recursive rule change 8 -> 42 | 42 8 (i.e. one or more 42s)
+    // and 11 -> 42 31 | 42 11 31 (i.e  a sequence of n 42 followed by n 31)
+    // Therefore rule 0 is now a sequence of n 42 followed by m 31
+    // where 2 < n and 0 < m < n
+    // The original solver does not work because it will only try the second branch
+    // if the first one fails.  Consider 8: 42 | 42 8.  Since the second starts the same
+    // as the first one it will never get called unless the first one failed, in which case
+    // it will fail as well, but what we want to check the second option as well as
+    // the first option. That is we need to be greedy on rule 42.
+    // We could try repeating rule 42 until it fails, and then try rule 31 up to
+    // n-1 times where n is the number of times we successfully matched rule 42.
+    // However this fails because 42 42 42 ? may fail at ? while 42 42 31 is valid
+    // Changing the rules to 8 -> 42 8 | 42 and 11 -> 31 11 | 11 will allow
+    // nx 42 followed by mx 31; but m will not be bound to n-1, and it will
+    // greedily consume the 42, when maybe it should stop ealier to look for 31.
+
+    guard let rule42 = rules[42] else { return false }
+    guard let rule31 = rules[31] else { return false }
+    guard !message.isEmpty else { return false }
+    self.message = message.reversed()
+    if !isMessageValid(rule: rule42) { return false }
+    var n = 1
+    while true {
+      let oldMessage = self.message
+      let failMode = found(count1: n, rule1: rule42, count2: n, rule2: rule31)
+      if failMode == .notn42 { return false }
+      // if failMode == .no31 { print("fail: no 31s; keep trying")}
+      if failMode == .ok {
+        if self.message.isEmpty { return true }
+      }
+      n += 1
+      self.message = oldMessage
+    }
+  }
+  enum FailModes {
+    case notn42
+    case no31
+    case ok
+  }
+
+  private func found(count1: Int, rule1: Rule, count2: Int, rule2: Rule) -> FailModes {
+    for _ in 1...count1 {
+      if !isMessageValid(rule: rule1) { return .notn42 }
+    }
+    for i in 1...count2 {
+      if !isMessageValid(rule: rule2) {
+        if i == 1 { return .no31 }
+      }
+    }
+    return .ok
   }
 
   private func isMessageValid(rule: Rule) -> Bool {
