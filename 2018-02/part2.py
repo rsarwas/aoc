07083ea7057222d68111d709d91fb624f4273047
Regan Sarwas <rsarwas@gmail.com>
2021-11-20 19:55:21 -0900

    2018-02 part 2 in Python

---
+++ 2018-02/part2.py
@@ -0,0 +1,30 @@
+import sys
+
+lines = []
+for line in sys.stdin:
+    lines.append(line)
+
+def common_string(s1, s2):
+    s = ""
+    for i in range(min(len(s1), len(s2))):
+        if s1[i] == s2[i]:
+            s += s1[i]
+    return s
+
+winner = ""
+for i in range(len(lines)-1):
+    for j in range(i+1,len(lines)):
+        common = common_string(lines[i], lines[j])
+        if len(common) > len(winner):
+            winner = common
+print(winner)
+
+""" Test Case = fgij
+abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz
+"""
