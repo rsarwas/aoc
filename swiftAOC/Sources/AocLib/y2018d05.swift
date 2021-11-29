0d59d6d18bc3336a72d0c400db6d58021b9275d4
Regan Sarwas <rsarwas@gmail.com>
2021-11-28 21:21:46 -0900

    Set up 2018 day 5 in Swift

---
+++ swiftAOC/Sources/AocLib/y2018d05.swift
@@ -0,0 +1,19 @@
+struct Problem201805: Problem {
+  var name: String { "2018-05" }
+  func solveWith(data: [String]) -> Solution { Solution201805(data: data) }
+}
+
+struct Solution201805: Solution {
+  let data: [String]
+
+  var part1: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+  var part2: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+}
--- swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
+++ swiftAOC/Tests/swiftAOCTests/swiftAOCTests.swift
@@ -123,30 +123,50 @@ final class swiftAOCTests: XCTestCase {
   //   }
   // }
 
-  func test201804b() {
+  // func test201804b() {
+  //   let testCases = [
+  //     ([
+  //       "[1518-11-01 00:00] Guard #10 begins shift",
+  //       "[1518-11-01 00:05] falls asleep",
+  //       "[1518-11-01 00:25] wakes up",
+  //       "[1518-11-01 00:30] falls asleep",
+  //       "[1518-11-01 00:55] wakes up",
+  //       "[1518-11-01 23:58] Guard #99 begins shift",
+  //       "[1518-11-02 00:40] falls asleep",
+  //       "[1518-11-02 00:50] wakes up",
+  //       "[1518-11-03 00:05] Guard #10 begins shift",
+  //       "[1518-11-03 00:24] falls asleep",
+  //       "[1518-11-03 00:29] wakes up",
+  //       "[1518-11-04 00:02] Guard #99 begins shift",
+  //       "[1518-11-04 00:36] falls asleep",
+  //       "[1518-11-04 00:46] wakes up",
+  //       "[1518-11-05 00:03] Guard #99 begins shift",
+  //       "[1518-11-05 00:45] falls asleep",
+  //       "[1518-11-05 00:55] wakes up"
+  //     ], "4455"),
+  //   ]
+  //   for (input, answer) in testCases {
+  //     let solution = Solution201804(data: input)
+  //     XCTAssertEqual(solution.part2, answer)
+  //   }
+  // }
+
+  func test201805a() {
     let testCases = [
-      ([
-        "[1518-11-01 00:00] Guard #10 begins shift",
-        "[1518-11-01 00:05] falls asleep",
-        "[1518-11-01 00:25] wakes up",
-        "[1518-11-01 00:30] falls asleep",
-        "[1518-11-01 00:55] wakes up",
-        "[1518-11-01 23:58] Guard #99 begins shift",
-        "[1518-11-02 00:40] falls asleep",
-        "[1518-11-02 00:50] wakes up",
-        "[1518-11-03 00:05] Guard #10 begins shift",
-        "[1518-11-03 00:24] falls asleep",
-        "[1518-11-03 00:29] wakes up",
-        "[1518-11-04 00:02] Guard #99 begins shift",
-        "[1518-11-04 00:36] falls asleep",
-        "[1518-11-04 00:46] wakes up",
-        "[1518-11-05 00:03] Guard #99 begins shift",
-        "[1518-11-05 00:45] falls asleep",
-        "[1518-11-05 00:55] wakes up"
-      ], "4455"),
+      (["dabAcCaCBAcCcaDA"], "10")
     ]
     for (input, answer) in testCases {
-      let solution = Solution201804(data: input)
+      let solution = Solution201805(data: input)
+      XCTAssertEqual(solution.part1, answer)
+    }
+  }
+
+  func test201805b() {
+    let testCases = [
+      (["dabAcCaCBAcCcaDA"], "10")
+    ]
+    for (input, answer) in testCases {
+      let solution = Solution201805(data: input)
       XCTAssertEqual(solution.part2, answer)
     }
   }
