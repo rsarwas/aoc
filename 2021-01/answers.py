9eea6c0318a261c42f063e2f0cc7c80a6acdc07a
Regan Sarwas <rsarwas@gmail.com>
2021-12-01 20:14:51 -0900

    Solve 2021-01 in Python
    < 12 minutes (but 1 day late)

--- 2021-01/answers.py
+++ 2021-01/answers.py
@@ -1,8 +1,22 @@
 def part1(lines):
-    return sum([int(line) for line in lines])
+    last_depth = 100000
+    inc = 0
+    for depth in [int(line) for line in lines]:
+        if depth > last_depth:
+            inc += 1
+        last_depth = depth
+    return inc
 
 def part2(lines):
-    return -1
+    last_depth = 100000
+    inc = 0
+    for i in range(0,len(lines)-2):
+        depth = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+        print(depth)
+        if depth > last_depth:
+            inc += 1
+        last_depth = depth
+    return inc
 
 if __name__ == '__main__':
     # data = open("input.txt").read() # as one big string
