d507788cdcdf75d29123a2eeb8656fbd22fb46ce
Regan Sarwas <rsarwas@gmail.com>
2021-12-01 21:29:15 -0900

    Solve 2021-01 in Swift

--- swiftAOC/Sources/AocLib/y2021d01.swift
+++ swiftAOC/Sources/AocLib/y2021d01.swift
@@ -7,13 +7,24 @@ struct Solution202101: Solution {
   let data: [String]
 
   var part1: String {
-    let answer = "Not Implemented"
+    let (answer, _) = data.compactMap { Int($0) }.reduce((0,Int.max)) { (acc, depth) in 
+      let (incCount, lastDepth) = acc
+      if depth > lastDepth { return (incCount + 1, depth) }
+      return (incCount, depth)
+    }
     return "\(answer)"
   }
 
   var part2: String {
-    let answer = "Not Implemented"
-    return "\(answer)"
+    let depths = data.compactMap { Int($0) }
+    var increases = 0
+    var lastDepth = Int.max
+    for (d1, (d2, d3)) in zip(depths, zip(depths[1...], depths[2...])) {
+      let depth = d1 + d2 + d3
+      if depth > lastDepth { increases += 1 }
+      lastDepth = depth
+    }
+    return "\(increases)"
   }
 
 }
