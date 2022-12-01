cbea83324e63b6fa3622d8c3ebd49695bc99943e
Regan Sarwas <rsarwas@gmail.com>
2022-12-01 10:05:20 -0900

    linting

--- 2022-02/answers.py
+++ 2022-02/answers.py
@@ -5,11 +5,12 @@
 def part1(lines):
     return -1
 
+
 def part2(lines):
     return -1
+
 
 if __name__ == '__main__':
     lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
-
--- 2022-05/answers.py
+++ 2022-05/answers.py
@@ -1,15 +1,38 @@
 # Data Model:
 # ===========
 # lines is a list of "\n" terminated strings from the input file
+# Each line is an integer or empty (just a newline)
+# There is no empty line at the end
 
 def part1(lines):
-    return -1
+    totals = addup_calories(lines)
+    return max(totals)
+
 
 def part2(lines):
-    return -1
+    totals = addup_calories(lines)
+    totals.sort()
+    top_three = totals[-3:]
+    return(sum(top_three))
+
+
+def addup_calories(lines):
+    totals = []
+    total_elf = 0
+    for line in lines:
+        line = line.strip()
+        if line:
+            # add this amount to the current elf's total
+            total_elf += int(line)
+        else:
+            totals.append(total_elf)
+            total_elf = 0
+    # there is no empty line at the end, so add the last elf
+    totals.append(total_elf)
+    return totals
+
 
 if __name__ == '__main__':
     lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
-
