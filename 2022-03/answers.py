b606098629ce61964f54265540defe054e7f08d1
Regan Sarwas <rsarwas@gmail.com>
2022-12-01 09:24:35 -0900

    prepare for 2022

---
+++ 2022-03/answers.py
@@ -0,0 +1,15 @@
+# Data Model:
+# ===========
+# lines is a list of "\n" terminated strings from the input file
+
+def part1(lines):
+    return -1
+
+def part2(lines):
+    return -1
+
+if __name__ == '__main__':
+    lines = open("input.txt").readlines()
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
+
