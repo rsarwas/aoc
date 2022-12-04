91f21161ba6cc3608d9034079dd5a074cf1c7265
Regan Sarwas <rsarwas@gmail.com>
2022-12-03 19:35:55 -0900

    Fix default file

--- 2022-03/answers.py
+++ 2022-03/answers.py
@@ -1,35 +1,14 @@
 # Data Model:
 # ===========
 # lines is a list of "\n" terminated strings from the input file
-# Each line is an integer or empty (just a newline)
-# There is no empty line at the end
+
 
 def part1(lines):
-    totals = addup_calories(lines)
-    return max(totals)
+    return -1
 
 
 def part2(lines):
-    totals = addup_calories(lines)
-    totals.sort()
-    top_three = totals[-3:]
-    return(sum(top_three))
-
-
-def addup_calories(lines):
-    totals = []
-    total_elf = 0
-    for line in lines:
-        line = line.strip()
-        if line:
-            # add this amount to the current elf's total
-            total_elf += int(line)
-        else:
-            totals.append(total_elf)
-            total_elf = 0
-    # there is no empty line at the end, so add the last elf
-    totals.append(total_elf)
-    return totals
+    return -1
 
 
 if __name__ == '__main__':
