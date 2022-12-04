3ab0cc11dc8510042340ab5e75f169a7d515e048
Regan Sarwas <rsarwas@gmail.com>
2022-12-03 20:27:24 -0900

    Solve 2022-04 part1 in python

--- 2022-04/answers.py
+++ 2022-04/answers.py
@@ -4,12 +4,33 @@
 
 
 def part1(lines):
-    return -1
+    pairs = parse(lines)
+    ps = proper_subsets(pairs)
+    return len(ps)
 
 
 def part2(lines):
     return -1
 
+
+def parse(lines):
+    pairs = []
+    for line in lines:
+        line = line.strip()
+        e1,e2 = line.split(",")
+        e1 = [int(i) for i in e1.split("-")]
+        e2 = [int(i) for i in e2.split("-")]
+        pairs.append((e1, e2))
+    return pairs
+
+
+def proper_subsets(pairs):
+    ps = []
+    for pair in pairs:
+        e1, e2 = pair
+        if (e1[0] >= e2[0] and e1[1] <= e2[1]) or (e1[0] <= e2[0] and e2[1] <= e1[1]):
+            ps.append(pair)
+    return ps
 
 if __name__ == '__main__':
     lines = open("input.txt").readlines()
