7866df146230603f9475832246de0c55bb83e05c
Regan Sarwas <rsarwas@gmail.com>
2022-12-03 19:53:10 -0900

    Solve 2022-03 part1 in python

--- 2022-03/answers.py
+++ 2022-03/answers.py
@@ -4,11 +4,34 @@
 
 
 def part1(lines):
-    return -1
+    items = parse(lines)
+    scores = [score(item) for item in items]
+    return sum(scores)
 
 
 def part2(lines):
     return -1
+
+def parse(lines):
+    items = []
+    for line in lines:
+        line = line.strip()
+        line_mid = len(line)//2
+        compartment1 = line[:line_mid]
+        compartment2 = line[line_mid:]
+        for item in compartment1:
+            if item in compartment2:
+                items.append(item)
+                break
+    return items
+
+
+def score(item):
+    """return 1..26 for a..z and 27..52 for A..Z"""
+    val = ord(item)
+    if val > 96: # a..
+        return val - 96
+    return val - 38
 
 
 if __name__ == '__main__':
