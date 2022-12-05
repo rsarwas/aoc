a1ec8f0bf252cf977ef9d0dea61b6ff946921e73
Regan Sarwas <rsarwas@gmail.com>
2022-12-04 21:08:39 -0900

    Solve 2022-05 part2 with Python

--- 2022-05/answers.py
+++ 2022-05/answers.py
@@ -4,16 +4,18 @@
 
 
 def part1(lines):
-    stacks, instructions = parse(lines)
-    reorg(stacks, instructions)
-    # print(stacks)
-    tops = [stack[-1] for stack in stacks]
-    # print(tops)
-    return "".join(tops)
+    return solve(lines, 9000)
 
 
 def part2(lines):
-    return -1
+    return solve(lines, 9001)
+
+
+def solve(lines, model):
+    stacks, instructions = parse(lines)
+    reorg(model, stacks, instructions)
+    tops = [stack[-1] for stack in stacks]
+    return "".join(tops)
 
 
 def parse(lines):
@@ -54,15 +56,24 @@ def make_stacks(stack_lines):
     return stacks
 
 
-def reorg(stacks, instructions):
+def reorg(model, stacks, instructions):
     for (n, src, dest) in instructions:
-        move(stacks[src-1], stacks[dest-1], n)
+        move(model, stacks[src-1], stacks[dest-1], n)
 
 
-def move(s1,s2,n):
-    for _ in range(n):
-        x = s1.pop()
-        s2.append(x)
+def move(model, s1,s2,n):
+    if model == 9000:
+        for _ in range(n):
+            x = s1.pop()
+            s2.append(x)
+    if model == 9001:
+        # print("before", s1,s2,n)
+        # s2 += s1[-n] # BAD makes a copy; using append puts a list in the list
+        s2[len(s2):] = s1[-n:] # add to a list in place
+        # s1 = s1[:-n] # BAD makes a copy
+        del s1[len(s1)-n:] # truncate a list in place
+        # print("after", s1, s2)
+
 
 if __name__ == '__main__':
     lines = open("input.txt").readlines()
