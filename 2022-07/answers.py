5027abbfdba027e9fa6c9a560851cbd0f8c8edf4
Regan Sarwas <rsarwas@gmail.com>
2022-12-06 22:09:59 -0900

    Solve 2022-07 part1

--- 2022-07/answers.py
+++ 2022-07/answers.py
@@ -1,23 +1,24 @@
 # Data Model:
 # ===========
 # lines is a list of "\n" terminated strings from the input file
-# fs is a dictionary with key = dir name (unique) and value = contents
+# fs is a dictionary with key = path (unique) and value = contents
+# path is a unix style path (i.e. parent_path/basename)
 # contents is a tuple (list of dir names, list of files)
 # file is a tuple (size (int), name (string))
 
 
 def part1(lines):
     fs = parse(lines)
-    print(fs)
     print(len(fs))
+    # print(len(fs))
     # fs_t = parse_test()
     # print(fs_t)
     sizes = {}
     get_sizes(fs, "/", sizes)
     total = 0
     print(len(sizes))
-    for size in sizes:
-        print("  ", size, sizes[size])
+    # for size in sizes:
+    #     print("  ", size, sizes[size])
     for d in sizes:
         if sizes[d] <= 100000:
             total += sizes[d]
@@ -34,15 +35,19 @@ def get_sizes(fs, root, sizes):
     for file in files:
         size += file[0]
     for d in dirs:
-        get_sizes(fs, d, sizes)
-        size += sizes[d]
+        if root == "/":
+            path = root + d
+        else:
+            path = root + "/" + d
+        get_sizes(fs, path, sizes)
+        size += sizes[path]
     sizes[root] = size
 
 
 def parse(lines):
     fs = {}
     listing = False
-    current_dir = None
+    path = ""
     dir_lines = []
     for line in lines:
         line = line.strip()
@@ -50,29 +55,38 @@ def parse(lines):
             if line.startswith("$ "):
                 # we are done collecting directory data
                 listing = False
-                if current_dir in fs:
-                    print("ERROR IN UNIQUE ASSUMPTION", current_dir)
-                fs[current_dir] = parse_dir(dir_lines)
-                dir_lines = []
-                #ignore the current line unles it is "$ cd {name}"
-                if line.startswith("$ cd ") and line != "$ cd ..":
-                    current_dir = line.replace("$ cd ", "")
+                if path in fs:
+                    print("ERROR - non unique path", path)
+                fs[path] = parse_dir(dir_lines)
+                # now process the current line should start with "$ cd "
+            else:
+                dir_lines.append(line)
                 continue
-            dir_lines.append(line)
-            continue
         if line == "$ ls":
             listing = True
+            dir_lines = []
             continue
         if line == "$ cd ..":
-            # ignore, it will always be followed by zero or more "$ cd .." then a "$ cd {name}"
+            # print("cd ..", path)
+            index = path.rindex("/")
+            path = path[:index]
+            if not path:
+                path = "/"
+            # print("  =>", path)
             continue
         if line.startswith("$ cd "):
             current_dir = line.replace("$ cd ", "")
-    # the last thing in the input is a directory listing, which needs to be parsed
-    if current_dir and dir_lines:
-        if current_dir in fs:
-            print("ERROR IN UNIQUE ASSUMPTION (last)", current_dir)
-        fs[current_dir] = parse_dir(dir_lines)
+            # print(line, path, current_dir)
+            if not path or path[-1] == "/":
+                path += current_dir
+            else:
+                path = path + "/" + current_dir
+            # print("  =>", path)
+    # At the EOF make sure we process the current listing
+    if listing and path and dir_lines:
+        if path in fs:
+            print("ERROR - non unique path at EOF", path)
+        fs[path] = parse_dir(dir_lines)
     return fs
 
 def parse_dir(lines):
@@ -90,9 +104,9 @@ def parse_dir(lines):
 def parse_test():
     d = {}
     d["/"] = (["a", "d"], [(14848514, "b.txt"), (8504156, "c.dat")])
-    d["a"] = (["e"], [(29116,"f"), (2557,"g"), (62596,"h.lst")])
-    d["e"] = ([],[(584,"i")])
-    d["d"] = ([], [(4060174,"j"),(8033020,"d.log"),(5626152,"d.ext"),(7214296,"k")])
+    d["/a"] = (["e"], [(29116,"f"), (2557,"g"), (62596,"h.lst")])
+    d["/a/e"] = ([],[(584,"i")])
+    d["/d"] = ([], [(4060174,"j"),(8033020,"d.log"),(5626152,"d.ext"),(7214296,"k")])
     return d
          
 
