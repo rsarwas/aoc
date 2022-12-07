7cedd88444a4234e8cdc1387376db122c5e5168d
Regan Sarwas <rsarwas@gmail.com>
2022-12-06 20:44:44 -0900

    2022-07 part1 step1
    develop data model and solver (test with pre-parsed input) 43 minutes

--- 2022-07/answers.py
+++ 2022-07/answers.py
@@ -1,15 +1,51 @@
 # Data Model:
 # ===========
 # lines is a list of "\n" terminated strings from the input file
+# fs is a dictionary with key = dir name (unique) and value = contents
+# contents is a tuple (list of dir names, list of files)
+# file is a tuple (size (int), name (string))
 
 
 def part1(lines):
-    return -1
+    fs = parse(lines)
+    print(fs)
+    fs_t = parse_test()
+    print(fs_t)
+    sizes = {}
+    get_sizes(fs_t, "/", sizes)
+    total = 0
+    # print(sizes)
+    for d in sizes:
+        if sizes[d] <= 100000:
+            total += sizes[d]
+    return total
 
 
 def part2(lines):
     return -1
 
+
+def get_sizes(fs, root, sizes):
+    dirs, files = fs[root]
+    size = 0
+    for file in files:
+        size += file[0]
+    for d in dirs:
+        get_sizes(fs, d, sizes)
+        size += sizes[d]
+    sizes[root] = size
+
+def parse(lines):
+    return {}
+
+def parse_test():
+    d = {}
+    d["/"] = (["a", "d"], [(14848514, "b.txt"), (8504156, "c.dat")])
+    d["a"] = (["e"], [(29116,"f"), (2557,"g"), (62596,"h.lst")])
+    d["e"] = ([],[(584,"i")])
+    d["d"] = ([], [(4060174,"j"),(8033020,"d.log"),(5626152,"d.ext"),(7214296,"k")])
+    return d
+         
 
 if __name__ == '__main__':
     lines = open("input.txt").readlines()
