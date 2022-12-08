8c381778127468872a4387343b16bab965f22489
Regan Sarwas <rsarwas@gmail.com>
2022-12-07 21:05:22 -0900

    Solve 2022-08 part2 in python

--- 2022-08/answers.py
+++ 2022-08/answers.py
@@ -11,7 +11,16 @@ def part1(lines):
 
 
 def part2(lines):
-    return -1
+    data = parse(lines)
+    # print(scenic_score(1,2,data))
+    # print(scenic_score(3,2,data))
+    best = 0
+    for r in range(len(data)):
+        for c in range(len(data[r])):
+            score = scenic_score(r,c,data)
+            if score > best:
+                best = score
+    return best
 
 
 def parse(lines):
@@ -64,6 +73,55 @@ def visible(data):
                 tallest[c] = height
     return result
 
+def scenic_score(r,c,data):
+    r_count = len(data)
+    c_count = len(data[0])
+    if r == 0 or c == 0 or c ==  c_count-1 or r == r_count-1:
+        # on an edge so one of the distance will be zero, so score is zero
+        return 0
+    height = data[r][c]
+    # look down; increase r to edge or tree of equal or greater height
+    down = 0
+    ri = r+1
+    while ri < r_count:
+        down += 1
+        if data[ri][c] >= height:
+            break
+        ri += 1
+    if down == 0:
+        return 0
+    # look down; decrease r to edge or tree of equal or greater height
+    up = 0
+    ri = r-1
+    while ri >= 0:
+        up += 1
+        if data[ri][c] >= height:
+            break
+        ri -= 1
+    if up == 0:
+        return 0
+    # look right; increase c to edge or tree of equal or greater height
+    right = 0
+    ci = c+1
+    while ci < c_count:
+        right += 1
+        if data[r][ci] >= height:
+            break
+        ci += 1
+    if right == 0:
+        return 0
+    # look left; decrease c to edge or tree of equal or greater height
+    left = 0
+    ci = c-1
+    while ci >= 0:
+        left += 1
+        if data[r][ci] >= height:
+            break
+        ci -= 1
+    if left == 0:
+        return 0    
+    # print(r, c, up, left, right, down)
+    return up*down*left*right
 
 def display(trees, size):
     grid = []
