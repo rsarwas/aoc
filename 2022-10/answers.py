10ed1fe4aefba58d74b86d4c1794ffe3ba3cd066
Regan Sarwas <rsarwas@gmail.com>
2022-12-09 20:24:11 -0900

    Solve 2022-10 part1 in python

--- 2022-10/answers.py
+++ 2022-10/answers.py
@@ -5,33 +5,46 @@
 
 def part1(lines):
     data = parse(lines)
-    result = solve(data)
+    # print(data)
+    x = 1
+    cycles = [20, 60, 100, 140, 180, 220]
+    result = solve(data, x, cycles)
     return result
 
 
 def part2(lines):
     data = parse(lines)
-    result = solve(data)
-    return result
+    # result = solve(data)
+    # return result
 
 
 def parse(lines):
     data = []
     for line in lines:
         line = line.strip()
-        item = line.split()
-        data.append(item)
+        if line == "noop":
+            data.append(None)
+        else:
+            _, val = line.split()
+            data.append(int(val))
     return data
 
 
-def solve(data):
-    result = 0
+def solve(data, x, cycles):
+    xs = [x]
     for item in data:
-        result += len(item)
-    return result
+        if item:
+            xs.append(x)
+            x += item
+            xs.append(x)
+        else:
+            xs.append(x)
+    # print(xs)
+    signals = [cycle * xs[cycle - 1] for cycle in cycles]
+    return sum(signals)
 
 
 if __name__ == '__main__':
-    lines = open("test.txt").readlines()
+    lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
