8b76b2bd2675a6fa4f1cdd65fbc0367d6b20ee5c
Regan Sarwas <rsarwas@gmail.com>
2022-12-17 21:25:26 -0900

    Incorrect solution to 2022-18 part2
    solves test, which has only a single cell void,  the real puzzle must have a multicell void

--- 2022-18/answers.py
+++ 2022-18/answers.py
@@ -11,7 +11,15 @@ def part1(lines):
 
 
 def part2(lines):
-    return -1
+    data = parse(lines)
+    result = solve(data)
+    # find all the single cell voids
+    voids = find_holes(data)
+    # find all the multi cell voids
+    # ???
+    interior_area = solve(voids)
+    result -= interior_area
+    return result
 
 
 def parse(lines):
@@ -43,6 +51,55 @@ def exposed(drop, drops):
         elif x1 == x and y1 == y and (z1 == z+1 or z1 == z-1):
             exposed -= 1
     return exposed
+
+
+def find_holes(drops):
+    x_min, y_min, z_min, x_max, y_max, z_max = extents(drops)
+    drop_set = set(drops)
+    voids = []
+    for x in range(x_min+1, x_max):
+        for y in range(y_min+1, y_max):
+                for z in range(z_min+1, z_max):
+                    if (x,y,z) in drop_set:
+                        continue
+                    if has_six_neighbors((x,y,z), drops):
+                        voids.append((x,y,z))
+    return voids
+
+def extents(drops):
+    x_max, y_max, z_max = (-1E6, -1E6, -1E6)
+    x_min, y_min, z_min = (1E6, 1E6, 1E6)
+    for drop in drops:
+        x,y,z = drop
+        if x < x_min:
+            x_min = x
+        if x > x_max:
+            x_max = x
+        if y < y_min:
+            y_min = y
+        if y > y_max:
+            y_max = y
+        if z < z_min:
+            z_min = z
+        if z > z_max:
+            z_max = z
+    return x_min, y_min, z_min, x_max, y_max, z_max
+
+
+def has_six_neighbors(void, drops):
+    x,y,z = void
+    neighbors = 0
+    for drop in drops:
+        x1,y1,z1 = drop
+        if y1 == y and z1 == z and (x1 == x+1 or x1 == x-1):
+            neighbors += 1
+        elif x1 == x and z1 == z and (y1 == y+1 or y1 == y-1):
+            neighbors += 1
+        elif x1 == x and y1 == y and (z1 == z+1 or z1 == z-1):
+            neighbors += 1
+    # if neighbors == 6:
+    #    print(void)
+    return neighbors == 6
 
 
 if __name__ == '__main__':
