b254fbc2e28b706088208ca983dbb3c3d0fa2bc3
Regan Sarwas <rsarwas@gmail.com>
2022-12-17 20:34:12 -0900

    Solve 2022-18 part 1 in Python

--- 2022-18/answers.py
+++ 2022-18/answers.py
@@ -5,33 +5,47 @@
 
 def part1(lines):
     data = parse(lines)
+    # data = [(1,1,1), (2,1,1)] # testg data; result should be 10
     result = solve(data)
     return result
 
 
 def part2(lines):
-    data = parse(lines)
-    result = solve(data)
-    return result
+    return -1
 
 
 def parse(lines):
     data = []
     for line in lines:
         line = line.strip()
-        item = line.split()
-        data.append(item)
+        items = line.split(",")
+        x,y,z = int(items[0]), int(items[1]), int(items[2])
+        data.append((x,y,z))
     return data
 
 
 def solve(data):
     result = 0
-    for item in data:
-        result += len(item)
+    for drop in data:
+        result += exposed(drop, data)
     return result
 
 
+def exposed(drop, drops):
+    x,y,z = drop
+    exposed = 6
+    for other in drops:
+        x1,y1,z1 = other
+        if y1 == y and z1 == z and (x1 == x+1 or x1 == x-1):
+            exposed -= 1
+        elif x1 == x and z1 == z and (y1 == y+1 or y1 == y-1):
+            exposed -= 1
+        elif x1 == x and y1 == y and (z1 == z+1 or z1 == z-1):
+            exposed -= 1
+    return exposed
+
+
 if __name__ == '__main__':
-    lines = open("test.txt").readlines()
+    lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
