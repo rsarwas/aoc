9dbc4f42d86c58fe534611e124329a210234b754
Regan Sarwas <rsarwas@gmail.com>
2022-12-20 21:19:05 -0900

    Solve 2022-21 part2 with Python

--- 2022-21/answers.py
+++ 2022-21/answers.py
@@ -5,12 +5,15 @@
 
 def part1(lines):
     data = parse(lines)
-    result = solve(data, "root")
+    result = solve(data, "root", 1, None)
     return result
 
 
 def part2(lines):
-    return -1
+    data = parse(lines)
+    # explore(data)
+    # return interpolate(data, 300, 400) # test
+    return interpolate(data, 3e12, 4e12) # problem
 
 
 def parse(lines):
@@ -30,22 +33,58 @@ def parse(lines):
     return data
 
 
-def solve(data, monkey):
+def solve(data, monkey, version, value):
+    if monkey == "humn" and version == 2:
+        return value
     job = data[monkey]
     if job[0] == 'yell':
         return job[1]
     else:
         op, monkey1, monkey2 = job
         if op == "*":
-            return solve(data,monkey1) * solve(data,monkey2)
+            return solve(data,monkey1, version, value) * solve(data,monkey2, version, value)
         if op == "/":
-            return solve(data,monkey1) / solve(data,monkey2)
+            return solve(data,monkey1, version, value) / solve(data,monkey2, version, value)
         if op == "+":
-            return solve(data,monkey1) + solve(data,monkey2)
+            return solve(data,monkey1, version, value) + solve(data,monkey2, version, value)
         if op == "-":
-            return solve(data,monkey1) - solve(data,monkey2)
+            return solve(data,monkey1, version, value) - solve(data,monkey2, version, value)
     print("PANIC. unexpected job")
     return -1
+
+
+def explore(data):
+    _, monkey1, monkey2 = data["root"]
+    # for val in [100, 200, 300, 400]:  # test puzzle
+    for val in [1e12, 2e12, 3e12, 4e12, 5e12]:
+        left = solve(data, monkey1, 2, val)
+        right = solve(data, monkey2, 2, val)
+        if left == right:
+            print("the winner is", val)
+        else:
+            verdict = "too small" if left < right else "too big" 
+            print("val", val, "is", verdict, "left", left, "right", right)
+
+
+def interpolate(data, lower, upper):
+    _, monkey1, monkey2 = data["root"]
+    val = (lower + upper) // 2
+    left = solve(data, monkey1, 2, val)
+    right = solve(data, monkey2, 2, val)
+    safety = 0
+    while left != right and safety < 50:
+        # print(upper, lower, left - right)
+        if left < right:
+            # lower = val # test left grows with larger inputs
+            upper = val # puzzle: left shrinks with larger inpu
+        else:
+            # upper = val # test left grows with larger inputs
+            lower = val # puzzle: left shrinks with larger inputs
+        val = (lower + upper) // 2
+        left = solve(data, monkey1, 2, val)
+        right = solve(data, monkey2, 2, val)
+        safety += 1
+    return val
 
 
 if __name__ == '__main__':
