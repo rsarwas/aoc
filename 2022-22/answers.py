6df911b50cf3bfb1c23083902e55c7ef4a01ef45
Regan Sarwas <rsarwas@gmail.com>
2022-12-23 22:57:34 -0900

    working code for 2022-22 part 2
    Not a solution; check tomorrow for bugs

--- 2022-22/answers.py
+++ 2022-22/answers.py
@@ -18,6 +18,17 @@ VOID = " "
 OPEN = "."
 WALL = "#"
 
+FACE_SIZE = 50
+R0 = 0
+R1 = 1 * FACE_SIZE
+R2 = 2 * FACE_SIZE
+R3 = 3 * FACE_SIZE
+R4 = 4 * FACE_SIZE
+C0 = 0
+C1 = 1 * FACE_SIZE
+C2 = 2 * FACE_SIZE
+C3 = 3 * FACE_SIZE
+
 # Used to change some internals deep in the code for part 2 
 PART = 1
 
@@ -34,6 +45,8 @@ def part2(lines):
     global PART
     PART = 2
     return part1(lines)
+    # first (wrong) run returned 145330, which is too low
+    # another wrong attempt returned 184163 which is too high
 
 
 def parse(lines):
@@ -139,7 +152,178 @@ def next_space(grid, loc, dir):
 
 
 def next_space2(grid, loc, dir):
-    return (loc, dir, True)
+    row, col = loc
+    dr, dc = dir
+    nrow, ncol = row + dr, col + dc
+    ndir = dir
+
+    in_face = face(row, col)
+    new_face = face(nrow, ncol)
+
+    if  new_face == None:
+        print("PANIC: I ended up off the cube")
+        return -1
+
+    if isinstance(new_face, int):
+        # the new face is on the map
+        pass
+    else:
+        # we have walked off the map, and need to
+        # recalibrate for the new face:
+        if new_face ==  (1,6):
+            nrow = col - R1 + C3
+            ncol = 0
+            ndir = RIGHT # up to right
+            if face(nrow, ncol) != 6: print("PANIC 1,6", in_face, face(nrow, ncol))
+        elif new_face == (2,6):
+            nrow = R4 - 1
+            ncol = col - C2
+            ndir = UP # up to up
+            if face(nrow, ncol) != 6: print("PANIC 2,6", in_face, face(nrow, ncol))
+        elif new_face == (1,4):
+            nrow = R3 - 1 - row
+            ncol = 0
+            ndir = RIGHT  # left to right
+            if face(nrow, ncol) != 4: print("PANIC 1,4", in_face, face(nrow, ncol))
+        elif new_face == (2,5):
+            nrow = R3 - 1 - row
+            ncol = C2 - 1
+            ndir = LEFT # right to left
+            if face(nrow, ncol) != 5: print("PANIC 2,5", in_face, face(nrow, ncol))
+        elif new_face == (3,4) and in_face == 3:
+            nrow = R2
+            ncol = row - R1
+            ndir = DOWN # left to down
+            if face(nrow, ncol) != 4: print("PANIC 3->4", in_face, face(nrow, ncol))
+        elif new_face == (3,4) and in_face == 4:
+            nrow = col + R1
+            ncol = C1
+            ndir = RIGHT # up to right
+            if face(nrow, ncol) != 3: print("PANIC 4->3", in_face, face(nrow, ncol))
+        elif new_face == (2,3) and in_face == 2:
+            nrow = R1 + col - C2
+            ncol = C2 - 1
+            ndir = LEFT # down to left
+            if face(nrow, ncol) != 3: print("PANIC 2->3", in_face, face(nrow, ncol))
+        elif new_face == (2,3) and in_face == 3:
+            nrow = R1 - 1
+            ncol = row - R1 + C2
+            ndir = UP  # right to up
+            if face(nrow, ncol) != 2: print("PANIC 3->2", in_face, face(nrow, ncol))
+        elif new_face == (4,1):
+            nrow = (R3 - 1) - row  # 149 -> 0; 100 -> 49
+            ncol = C1
+            ndir = RIGHT # left to right
+            if face(nrow, ncol) != 1: print("PANIC 4,1", in_face, face(nrow, ncol))
+        elif new_face == (5,2):
+            nrow = R3 - row
+            ncol = C3 - 1
+            ndir = LEFT # right to left
+            if face(nrow, ncol) != 2: print("PANIC 5,2", in_face, face(nrow, ncol))
+        elif new_face == (6,1):
+            nrow = 0
+            ncol = row - R3 + C1
+            ndir = DOWN  # left to down
+            if face(nrow, ncol) != 1: print("PANIC 6,1", in_face, face(nrow, ncol))
+        elif new_face == (5,6) and in_face == 5:
+            nrow = col - C1 + R3
+            ncol = C1 - 1
+            ndir = LEFT # down to left
+            if face(nrow, ncol) != 6: print("PANIC 5->6", in_face, face(nrow, ncol))
+        elif new_face == (5,6) and in_face == 6:
+            nrow = R3 - 1
+            ncol = row - R3 + C1
+            ndir = UP # right to up
+            if face(nrow, ncol) != 5: print("PANIC 6->5", in_face, face(nrow, ncol))
+        elif new_face == (6,2):
+            nrow = 0
+            ncol = col + C2
+            ndir = DOWN # down to down
+            if face(nrow, ncol) != 2: print("PANIC 6,2", in_face, face(nrow, ncol))
+    # print(row, col, nrow, ncol, in_face, new_face, face(nrow, ncol))
+    char = grid[nrow][ncol]
+    if char == WALL:
+        return ((row, col), dir, True)
+    if char == OPEN:
+        return ((nrow, ncol), ndir, False)
+    
+    print("PANIC: we are not on the cube anymore")
+
+
+def face(row, col):
+    # this is specific to my puzzle input, it does not match the test input
+    # it is also based on my arbitrary face numbering
+    # the tuples in or outside the grid are the invalid location that
+    # transport you to a different face, usually it is (from,to),
+    # but both (3,4), (2,3) and (5,6) are two way, so we need the starting face
+    #
+    #      -1     0     1     2     3
+    #  -1             (1,6) (2,6)
+    #   0       (1,4)   1     2   (2,5)
+    #   1       (3,4)   3   (2,3)
+    #   2 (4,1)   4     5   (5,2)
+    #   3 (6,1)   6   (5,6)   x
+    #   4       (6,2)
+    #
+    # where each face is 50x50 and row and columns indices start at 0
+
+    if row < R0:
+        if col < C1:
+            return None
+        if col < C2:
+            return (1, 6)
+        if  col < C3:
+            return (2, 6)
+        return None
+
+    if row < R1:
+        if col < C0:
+            return None
+        if col < C1:
+            return (1,4)
+        if col < C2:
+            return 1
+        if col < C3:
+            return 2
+        return (2,5)
+
+    if row < R2:
+        if col < C0:
+            return None
+        if col < C1:
+            return (3,4)
+        if col < C2:
+            return 3
+        if col < C3:
+            return (2,3)
+        return None
+
+    if row < R3:
+        if col < C0:
+            return (4,1)
+        if col < C1:
+            return 4
+        if col < C2:
+            return 5
+        if col < C3:
+            return (5,6)
+        return None
+
+    if row < R4:
+        if col < C0:
+            return (6,1)
+        if col < C1:
+            return 6
+        if col < C2:
+            return (5,6)
+        return None
+
+    # Row 4 or higher:
+    if col < C0:
+        return None
+    if col < C1:
+        return (6,2)
+    return None
 
 
 def inside(grid, row, col):
@@ -185,7 +369,7 @@ def password(loc, dir):
 
 
 if __name__ == '__main__':
-    lines = open("test.txt").readlines()
+    lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
 
