78a480e4fe94bc5f7ce559963969e40a8565a63d
Regan Sarwas <rsarwas@gmail.com>
2022-12-24 21:02:50 -0900

    Solve 2022-25 in Python

--- 2022-25/answers.py
+++ 2022-25/answers.py
@@ -4,34 +4,52 @@
 
 
 def part1(lines):
-    data = parse(lines)
-    result = solve(data)
-    return result
-
-
-def part2(lines):
-    data = parse(lines)
-    result = solve(data)
-    return result
-
-
-def parse(lines):
-    data = []
+    total = 0
     for line in lines:
-        line = line.strip()
-        item = line.split()
-        data.append(item)
-    return data
+        total += to_decimal(line.strip())
+    return to_snafu(total)
 
 
-def solve(data):
+SNAFU = {
+    "2":2,
+    "1":1,
+    "0":0,
+    "-":-1,
+    "=":-2
+}
+
+DECIMAL = {
+    2:"2",
+    1:"1",
+    0:"0",
+    -1:"-",
+    -2:"="
+}
+
+def to_decimal(snafu):
     result = 0
-    for item in data:
-        result += len(item)
-    return result
+    factor = 5**(len(snafu)-1)
+    for char in snafu:
+        result += factor * SNAFU[char]
+        factor //= 5
+    return result 
+
+def to_snafu(decimal):
+    quot = decimal // 5
+    rem = decimal % 5
+    digits = []
+    while quot > 0 or rem > 2:
+        if rem > 2:
+            rem -= 5
+            quot += 1
+        digits.append(DECIMAL[rem])
+        rem = quot % 5
+        quot //= 5
+    digits.append(DECIMAL[rem])
+    digits.reverse()
+    return("".join(digits))
 
 
 if __name__ == '__main__':
-    lines = open("test.txt").readlines()
+    lines = open("input.txt").readlines()
     print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
