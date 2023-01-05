f20bbb91011ebaf69aa50ca481e7a8ec5ea8c8db
Regan Sarwas <rsarwas@gmail.com>
2023-01-04 16:55:10 -0900

    Solve 2022-20 part1 with a doubly linked list

--- 2022-20/answers.py
+++ 2022-20/answers.py
@@ -5,19 +5,8 @@
 # lines is a list of "\n" terminated strings from the input file
 # each line is a random integer in range {-1e5 ,.., 1e5}; integers are NOT unique
 # so we cannot search for the number that needs to move; we need to keep track of
-# it's current location.  Therefore the following simple brute force solution will
-# not work: find n, then swap it n times with the element after (or before for negative n)
-# Puzzle specifies that list is "circular", so moving a number off one end of the
-# list wraps back around to the other end as if the ends were connected.
-# it isn't clear what should happen if the move is so large that it passes the
-# original location. i.e. does it step wise swap with it's neighbor n times, so
-# number may move multiple spaces for each loop around the list, or is it a
-# enough to figure the final resting place (distance mod list length), and then
-# move each intervening number once.  The puzzle input is 5000 numbers, with many
-# numbers greater than +/-5000, so this situation will occur many times.
-
-# Current solution works correctly with the test data
-# but yields incorrect answer of 4326 (too low) for part1.
+# it's current location. Solution is to put each integer in a doubly linked list.
+# The linked list will be stored as tuples in a standard python list.
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
@@ -28,9 +17,10 @@ DEBUGGING = False
 
 def part1(lines):
     """Solve part 1 of the puzzle."""
-    data = parse(lines)
-    data = mix(data)
-    result = gps_code(data, 0)
+    ints = parse(lines)
+    ddl = make_doubly_linked_list(ints)
+    ddl = mix(ddl)
+    result = gps_code(ddl, 0)
     return result
 
 
@@ -45,131 +35,121 @@ def parse(lines):
     return data
 
 
-def mix(data):
-    """Mix up the data by shifting each number to a new location in the list.
-    The value of the number is the distance to move it (left for negative numbers).
-    The list is circular.  If a number ends up between the first and last,
-    it goes at the end."""
+def make_doubly_linked_list(ints):
+    """Given a list of ints, return a doubly linked list with the ints.
+    The data structure is a standard python list with each entry being
+    a (prev, value, next) tuple, where prev and next are the indexes in
+    the list of the previous and next items in the list. items do not
+    in the standard list."""
+    ddl = []
+    size = len(ints)
+    for i, value in enumerate(ints):
+        prev = (i - 1) % size
+        next_ = (i + 1) % size
+        item = (prev, value, next_)
+        ddl.append(item)
+    return ddl
 
+
+def mix(ddl):
+    """Mix up the doubly linked list (ddl) by shifting each item to a new location
+    in the list. The value of the item is the distance to move it (left for negative
+    numbers). The list is circular.
+    Unclear if the item should be removed from the list before finding the new location,
+    or after.  This only applies to abs(value) > len(ddl), but that never happens in
+    the test input, but happens about 50% of the time in the puzzle input. Removing the
+    item from the list _BEFORE_ finding the new location returns the correct answer!"""
     if DEBUGGING:
-        print("Initial arrangement:")
-        print(data)
-
-    indexes = list(range(len(data)))  # starts the same as the original indexes
-    new_data = list(data)  # will be updated at the end with the reorganized list
-    size = len(data)
-    for index, value in enumerate(data):
-        index = indexes[index]  # current location of value
-        left = index + value
-        if value < 0:
-            left -= 1
-        left %= size
-
+        display(ddl)
+    for ptr, item in enumerate(ddl):
+        offset = item[1]
+        # Removing the item from the list _BEFORE_ finding the new location
+        # returns the correct answer!
+        remove(ddl, ptr)
+        before = get_previous_ptr(ddl, ptr, offset)
+        add(ddl, ptr, before)
         if DEBUGGING:
-            right = left + 1
-            right %= size
-            # print(index, left, right)
-            # print(indexes)
-            left_value, right_value = None, None
-            for i, i_value in enumerate(indexes):
-                if i_value == left:
-                    left_value = data[i]
-                if i_value == right:
-                    right_value = data[i]
-            print(f"\n{value} moves between {left_value} and {right_value}")
+            display(ddl)
+    return ddl
 
-        # if left == index or left == index - 1:
-        # Move the number to the same spot, so do nothing
-        if left < index - 1:  # shift numbers to the right
-            for i, i_value in enumerate(indexes):
-                if left < i_value <= index:
-                    indexes[i] = i_value + 1
-                if i_value == index:
-                    indexes[i] = left + 1  # aka right
-        elif index < left:  # shift numbers to the left
-            for i, i_value in enumerate(indexes):
-                if index < i_value <= left:
-                    indexes[i] = i_value - 1
-                if i_value == index:
-                    indexes[i] = left
 
+def get_previous_ptr(ddl, ptr, offset):
+    """Returns the left side (node before) the location that
+    offset moves from the location of the node at ptr.
+    offset can be positive or negative."""
+    prev = ddl[ptr][0]
+    next_ = ddl[ptr][2]
+    while offset > 1:
+        next_ = ddl[next_][2]
+        offset -= 1
+    if offset == 1:
+        return next_
+    while offset < 0:
+        prev = ddl[prev][0]
+        offset += 1
+    return prev
+
+
+def remove(ddl, ptr):
+    """Remove the item at ptr from the doubly linked list."""
+    prev = ddl[ptr][0]
+    next_ = ddl[ptr][2]
+    update_prev(ddl, next_, prev)
+    update_next(ddl, prev, next_)
+
+
+def add(ddl, ptr, after):
+    """Add the doubly linked list node at ptr to the spate after the node at after.
+    the node in ptr is assumed in the data structure (a standard python list), but
+    not necessarily connected to the next/prev items in the list."""
+    next_ = ddl[after][2]
+    prev = after
+    update_next(ddl, prev, ptr)
+    update_prev(ddl, ptr, prev)
+    update_next(ddl, ptr, next_)
+    update_prev(ddl, next_, ptr)
+
+
+def update_prev(ddl, ptr, prev):
+    """Update the previous pointer of the item at ptr in ddl to prev."""
+    ddl[ptr] = (prev, ddl[ptr][1], ddl[ptr][2])
+
+
+def update_next(ddl, ptr, next_):
+    """Update the next pointer of the item at ptr in ddl to next."""
+    ddl[ptr] = (ddl[ptr][0], ddl[ptr][1], next_)
+
+
+def gps_code(ddl, value):
+    """Get the sum of three values in the doubly linked list.
+    Values are at index of value + 1000, 2000, and 3000."""
+    ptr = find_value(ddl, value)
+    result = 0
+    for _ in range(3):
+        ptr = get_previous_ptr(ddl, ptr, 1000)
         if DEBUGGING:
-            # Generate the data in the current order for printing
-            for i, index in enumerate(indexes):
-                new_data[index] = data[i]
-            print(new_data)
-
-    # Return the data in the current ordering
-    for i, index in enumerate(indexes):
-        new_data[index] = data[i]
-    return new_data
+            print(ddl[ptr][1])
+        result += ddl[ptr][1]
+    return result
 
 
-def gps_code(data, value):
-    """Get the sum of three values in the rearranged list.
-    Values are at index of val + 1000, 2000, and 3000."""
-    code = 0
-    length = len(data)
-    v_index = data.index(value)
-    for i in [1000, 2000, 3000]:
-        index = (v_index + i) % length
-        code += data[index]
-    return code
+def find_value(ddl, value):
+    """Find the pointer to the node in ddl containing value."""
+    for i, item in enumerate(ddl):
+        if item[1] == value:
+            return i
+    return None
 
 
-def test_uniqueness(lines):
-    """Test if the integers in the input list are unique. For debugging"""
-    data = parse(lines)
-    print("Values are unique:", len(set(data)) == len(data))
-
-
-def test_indexing():
-    """Test the indexing algorithm. For debugging"""
-    test_indexing2([1, 2, 3, 1, 7])
-    # [1, 2, 3, 1, 7]
-    # 1 moves between 2 and 3 => [2, 1, 3, 1, 7]
-    # 2 moves between 1 and 7 => [1, 3, 1, 2, 7]
-    # 3 moves between 1 and 2 => [1, 3, 2, 1, 7]
-    # 1 moves between 7 and 1 => [1, 2, 3, 7, 1]
-    # 7 moves between 2 and 3 => [1, 2, 7, 3, 1]
-    test_indexing2([-7, -1, -3, -2, -1])
-    # [-7, -1, -3, -2, -1]
-    # -7 moves between -3 and -2 => [-1, -3, -7, -2, -1]
-    # -1 moves between -1 and -7 => [-7, -3, -2, -1, -1]
-    # -3 moves between -2 and -1 => [-7, -1, -2, -3, -1]
-    # -2 moves between -7 and -1 => [-7, -2, -1, -3, -1]
-    # -1 moves between -3 and -2 => [-7, -1, -3, -1, -2]
-
-
-def test_indexing2(data):
-    """Test the indexing algorithm. For debugging"""
-    print(data)
-    size = len(data)
-    for index, value in enumerate(data):
-        left = index + value
-        if value < 0:
-            left -= 1
-        left %= size
-        right = left + 1
-        right %= size
-        # print(index, left, right)
-
-        # right will always be greater than left
-        #   unless left is the last element then right is 0
-
-        new_data = list(data)
-        # if left == index or left == index - 1:
-        # Move the number to the same spot, so do nothing
-        if left < index - 1:  # shift numbers to the right
-            for i in range(index, left, -1):
-                new_data[i] = data[i - 1]
-            new_data[right] = value
-        elif index < left:  # shift numbers to the left
-            for i in range(index + 1, left + 1):
-                new_data[i - 1] = data[i]
-            new_data[left] = value
-
-        print(f"{value} moves between {data[left]} and {data[right]} => {new_data}")
+def display(ddl):
+    """Prints the doubly linked list in the correct order.
+    The node at index 0 is always printed first."""
+    output = [ddl[0][1]]
+    ptr = ddl[0][2]
+    while ptr != 0:
+        output.append(ddl[ptr][1])
+        ptr = ddl[ptr][2]
+    print(output)
 
 
 def main(filename):
@@ -178,13 +158,9 @@ def main(filename):
     with open(filename, encoding="utf8") as data:
         lines = data.readlines()
     print(f"Solving Advent of Code {puzzle} with {filename}")
-    # test_uniqueness(lines)
     print(f"Part 1: {part1(lines)}")
     print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
     main(INPUT)
-    # test_indexing()
-    # mix([5, 1, 0, 14, 25, 5, 9, 1])
-    # mix([1, 0, -1, -10, -5, -2, -1])
---
+++ 2022-20/answers_old.py
@@ -0,0 +1,190 @@
+"""A solution to an Advent of Code puzzle."""
+
+# Data Model:
+# ===========
+# lines is a list of "\n" terminated strings from the input file
+# each line is a random integer in range {-1e5 ,.., 1e5}; integers are NOT unique
+# so we cannot search for the number that needs to move; we need to keep track of
+# it's current location.  Therefore the following simple brute force solution will
+# not work: find n, then swap it n times with the element after (or before for negative n)
+# Puzzle specifies that list is "circular", so moving a number off one end of the
+# list wraps back around to the other end as if the ends were connected.
+# it isn't clear what should happen if the move is so large that it passes the
+# original location. i.e. does it step wise swap with it's neighbor n times, so
+# number may move multiple spaces for each loop around the list, or is it a
+# enough to figure the final resting place (distance mod list length), and then
+# move each intervening number once.  The puzzle input is 5000 numbers, with many
+# numbers greater than +/-5000, so this situation will occur many times.
+
+# Current solution works correctly with the test data
+# but yields incorrect answer of 4326 (too low) for part1.
+
+import os.path  # to get the directory name of the script (current puzzle year-day)
+
+INPUT = "input.txt"
+
+DEBUGGING = False
+
+
+def part1(lines):
+    """Solve part 1 of the puzzle."""
+    data = parse(lines)
+    data = mix(data)
+    result = gps_code(data, 0)
+    return result
+
+
+def part2(lines):
+    """Solve part 2 of the puzzle."""
+    return -1
+
+
+def parse(lines):
+    """Parse the puzzle input file into a usable data structure."""
+    data = [int(line) for line in lines]
+    return data
+
+
+def mix(data):
+    """Mix up the data by shifting each number to a new location in the list.
+    The value of the number is the distance to move it (left for negative numbers).
+    The list is circular.  If a number ends up between the first and last,
+    it goes at the end."""
+
+    if DEBUGGING:
+        print("Initial arrangement:")
+        print(data)
+
+    indexes = list(range(len(data)))  # starts the same as the original indexes
+    new_data = list(data)  # will be updated at the end with the reorganized list
+    size = len(data)
+    for index, value in enumerate(data):
+        index = indexes[index]  # current location of value
+        left = index + value
+        if value < 0:
+            left -= 1
+        left %= size
+
+        if DEBUGGING:
+            right = left + 1
+            right %= size
+            # print(index, left, right)
+            # print(indexes)
+            left_value, right_value = None, None
+            for i, i_value in enumerate(indexes):
+                if i_value == left:
+                    left_value = data[i]
+                if i_value == right:
+                    right_value = data[i]
+            print(f"\n{value} moves between {left_value} and {right_value}")
+
+        # if left == index or left == index - 1:
+        # Move the number to the same spot, so do nothing
+        if left < index - 1:  # shift numbers to the right
+            for i, i_value in enumerate(indexes):
+                if left < i_value <= index:
+                    indexes[i] = i_value + 1
+                if i_value == index:
+                    indexes[i] = left + 1  # aka right
+        elif index < left:  # shift numbers to the left
+            for i, i_value in enumerate(indexes):
+                if index < i_value <= left:
+                    indexes[i] = i_value - 1
+                if i_value == index:
+                    indexes[i] = left
+
+        if DEBUGGING:
+            # Generate the data in the current order for printing
+            for i, index in enumerate(indexes):
+                new_data[index] = data[i]
+            print(new_data)
+
+    # Return the data in the current ordering
+    for i, index in enumerate(indexes):
+        new_data[index] = data[i]
+    return new_data
+
+
+def gps_code(data, value):
+    """Get the sum of three values in the rearranged list.
+    Values are at index of val + 1000, 2000, and 3000."""
+    code = 0
+    length = len(data)
+    v_index = data.index(value)
+    for i in [1000, 2000, 3000]:
+        index = (v_index + i) % length
+        code += data[index]
+    return code
+
+
+def test_uniqueness(lines):
+    """Test if the integers in the input list are unique. For debugging"""
+    data = parse(lines)
+    print("Values are unique:", len(set(data)) == len(data))
+
+
+def test_indexing():
+    """Test the indexing algorithm. For debugging"""
+    test_indexing2([1, 2, 3, 1, 7])
+    # [1, 2, 3, 1, 7]
+    # 1 moves between 2 and 3 => [2, 1, 3, 1, 7]
+    # 2 moves between 1 and 7 => [1, 3, 1, 2, 7]
+    # 3 moves between 1 and 2 => [1, 3, 2, 1, 7]
+    # 1 moves between 7 and 1 => [1, 2, 3, 7, 1]
+    # 7 moves between 2 and 3 => [1, 2, 7, 3, 1]
+    test_indexing2([-7, -1, -3, -2, -1])
+    # [-7, -1, -3, -2, -1]
+    # -7 moves between -3 and -2 => [-1, -3, -7, -2, -1]
+    # -1 moves between -1 and -7 => [-7, -3, -2, -1, -1]
+    # -3 moves between -2 and -1 => [-7, -1, -2, -3, -1]
+    # -2 moves between -7 and -1 => [-7, -2, -1, -3, -1]
+    # -1 moves between -3 and -2 => [-7, -1, -3, -1, -2]
+
+
+def test_indexing2(data):
+    """Test the indexing algorithm. For debugging"""
+    print(data)
+    size = len(data)
+    for index, value in enumerate(data):
+        left = index + value
+        if value < 0:
+            left -= 1
+        left %= size
+        right = left + 1
+        right %= size
+        # print(index, left, right)
+
+        # right will always be greater than left
+        #   unless left is the last element then right is 0
+
+        new_data = list(data)
+        # if left == index or left == index - 1:
+        # Move the number to the same spot, so do nothing
+        if left < index - 1:  # shift numbers to the right
+            for i in range(index, left, -1):
+                new_data[i] = data[i - 1]
+            new_data[right] = value
+        elif index < left:  # shift numbers to the left
+            for i in range(index + 1, left + 1):
+                new_data[i - 1] = data[i]
+            new_data[left] = value
+
+        print(f"{value} moves between {data[left]} and {data[right]} => {new_data}")
+
+
+def main(filename):
+    """Solve both parts of the puzzle."""
+    _, puzzle = os.path.split(os.path.dirname(__file__))
+    with open(filename, encoding="utf8") as data:
+        lines = data.readlines()
+    print(f"Solving Advent of Code {puzzle} with {filename}")
+    # test_uniqueness(lines)
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
+
+
+if __name__ == "__main__":
+    main(INPUT)
+    # test_indexing()
+    # mix([5, 1, 0, 14, 25, 5, 9, 1])
+    # mix([1, 0, -1, -10, -5, -2, -1])
