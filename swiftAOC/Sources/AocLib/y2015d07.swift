341bcc440182978456ef3ce8c00afd01bb2ffdb4
Regan Sarwas <rsarwas@gmail.com>
2023-11-24 11:49:28 -0900

    swift 2015-07 setup

---
+++ swiftAOC/Sources/AocLib/y2015d07.swift
@@ -0,0 +1,19 @@
+struct Problem201507: Problem {
+  var name: String { "2015-07" }
+  func solveWith(data: [String]) -> Solution { Solution201507(data: data) }
+}
+
+struct Solution201507: Solution {
+  let data: [String]
+
+  var part1: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+  var part2: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+}
