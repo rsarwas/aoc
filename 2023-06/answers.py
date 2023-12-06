a153827510ab07b08a0b43ae81708772c6051d3f
Regan Sarwas <rsarwas@gmail.com>
2023-12-06 12:58:32 -0900

    Solve 2023-05 in python with Math

--- 2023-06/answers.py
+++ 2023-06/answers.py
@@ -15,14 +15,14 @@ def part1(lines):
     times, distances = parse(lines)
     total = 1
     for race in zip(times, distances):
-        total *= ways_to_win_simple(race)
+        total *= ways_to_win_math(race)
     return total
 
 
 def part2(lines):
     """Solve part 2 of the problem."""
     race = parse2(lines)
-    total = ways_to_win_simple(race)
+    total = ways_to_win_math(race)
     return total
 
 
@@ -67,6 +67,46 @@ def ways_to_win_simple(race):
     for speed in range(1, time):
         if (time - speed) * speed > distance:
             wins += 1
+    return wins
+
+
+def ways_to_win_math(race):
+    """Find the number of speeds that will win the race
+    time = duration of the race, distance = record distance
+    We win if we pick a speed that results in a distance greater than the record
+
+    We win iff (time - hold_time) * speed > distance
+    solve for v = hold_time = speed in (time - v) * v > distance
+    rearranging: v^2 - time*v + distance < 0
+    use the quadratic equation x = [-b +/- sqrt(b^2 -4ac)] / 2 to solve for v.
+    These will be the upper and lower winning speeds.  The number of wins is the
+    integers between them.  If one of the bounds is an integer than it does not
+    count (it would be a tie).
+    note: simplifying the difference to just sqrt(time * time - 4 * distance)
+    makes it impossible to determine if either end is an integer
+
+    Analysis: this took a lot longer to work out the math, and ensure it was
+    reasoned correctly, and then the code is a lot longer, and the boundary
+    conditions were tricky.  This took way longer writing code to solve the puzzle
+    in 0 seconds vs. 5 seconds.  A case of premature optimization."""
+
+    import math
+
+    time, distance = race
+    x = math.sqrt(time * time - 4 * distance)
+    v_max = (time + x) / 2
+    v_min = (time - x) / 2
+    e = 1e-8
+    if abs(v_max - int(v_max)) < e:
+        v_max = int(v_max) - 1
+    else:
+        v_max = int(v_max)
+    if abs(v_min - int(v_min + 1)) < e:
+        v_min = int(v_min + 1) + 1
+    else:
+        v_min = int(v_min + 1)
+    wins = v_max - v_min + 1
+    # print(v_max, v_min, wins)
     return wins
 
 
