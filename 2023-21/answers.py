da8e6c8c4e4b2c2ec88ded058912556cf9954c00
Regan Sarwas <rsarwas@gmail.com>
2023-12-20 21:33:31 -0900

    Solve 2023-21 part 1 with Python

---
+++ 2023-21/answers.py
@@ -0,0 +1,89 @@
+"""A solution to an Advent of Code puzzle."""
+
+# Data Model:
+# ===========
+# _lines_ is a list of "\n" terminated strings from the input file.
+
+
+import os.path  # to get the directory name of the script (current puzzle year-day)
+
+INPUT = "input.txt"
+OPEN = "."
+ROCK = "#"
+START = "S"
+
+
+def part1(lines):
+    """Solve part 1 of the problem."""
+    start, rocks, extents = parse(lines)
+    steps = 64
+    tiles = take_steps(start, rocks, extents, steps)
+    total = len(tiles)
+    return total
+
+
+def part2(lines):
+    """Solve part 2 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
+
+
+def parse(lines):
+    """Convert the lines of text into a useful data model.
+    Return a set of all the locations (row,col) of rocks (no-go tiles)
+    and the extents, (max_row, max_col), and the start (row, col)"""
+    max_rows = len(lines)
+    max_cols = len(lines[0].strip())
+    extents = (max_rows, max_cols)
+    rocks = set()
+    start = None
+    for row, line in enumerate(lines):
+        for col, char in enumerate(line.strip()):
+            if char == START:
+                start = (row, col)
+            if char == ROCK:
+                rocks.add((row, col))
+    return start, rocks, extents
+
+
+def take_steps(start, rocks, extents, steps):
+    """Return all the possible tiles that could be the final
+    resting place after taking steps from start"""
+    origins = [start]
+    for _ in range(steps):
+        destinations = set()
+        for location in origins:
+            for neighbor in valid_neighbors(location, rocks, extents):
+                destinations.add(neighbor)
+        origins = destinations
+    return destinations
+
+
+def valid_neighbors(location, rocks, extents):
+    """Return all the locations adjacent to location (up, down, left, right)
+    that are on the grid GTE (0,0) and LT extents, and not a rock"""
+    old_row, old_col = location
+    max_row, max_col = extents
+    neighbors = []
+    for row in [old_row - 1, old_row + 1]:
+        if row >= 0 and row < max_row and (row, old_col) not in rocks:
+            neighbors.append((row, old_col))
+    for col in [old_col - 1, old_col + 1]:
+        if col >= 0 and col < max_col and (old_row, col) not in rocks:
+            neighbors.append((old_row, col))
+    return neighbors
+
+
+def main(filename):
+    """Solve both parts of the puzzle."""
+    _, puzzle = os.path.split(os.path.dirname(__file__))
+    with open(filename, encoding="utf8") as data:
+        lines = data.readlines()
+    print(f"Solving Advent of Code {puzzle} with {filename}")
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
+
+
+if __name__ == "__main__":
+    main(INPUT)
