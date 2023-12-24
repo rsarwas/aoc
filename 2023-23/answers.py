00cff5f97ffba4b7d37940129105f77600a6ca6e
Regan Sarwas <rsarwas@gmail.com>
2023-12-23 22:56:09 -0900

    Build an ADG from the input in 2023-23

---
+++ 2023-23/answers.py
@@ -0,0 +1,202 @@
+"""A solution to an Advent of Code puzzle."""
+
+# Data Model:
+# ===========
+# _lines_ is a list of "\n" terminated strings from the input file.
+
+
+import os.path  # to get the directory name of the script (current puzzle year-day)
+
+INPUT = "input.txt"
+WALL = "#"
+DOWN = "v"
+UP = "^"
+RIGHT = ">"
+LEFT = "<"
+
+
+def part1(lines):
+    """Solve part 1 of the problem."""
+    maze = parse(lines)
+    graph = make_adg(maze)
+    total = len(graph)
+    return total
+
+
+def part2(lines):
+    """Solve part 2 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
+
+
+def parse(lines):
+    """Convert the lines of text into a useful data model."""
+    return [line.strip() for line in lines]
+
+
+def make_adg(maze):
+    """Return a Acyclic Directed Graph from the maze.
+    nodes is a list of locations (row, col), the list index is the node_id,
+    edges are (start_node_id, end_node_id, edge_length) tuples,"""
+    path = make_path(maze)
+    start, end = find_ends(maze)
+    junctions = make_junctions(start, end, path)
+    nodes = make_nodes(junctions)
+    # for i, n in enumerate(nodes):
+    #     print(f"{i}: {n}")
+    edges = make_edges(junctions, path)
+    edges = simplify_edges(edges, nodes)
+    # for edge in edges:
+    #     print(edge)
+    return nodes, edges
+
+
+def make_path(maze):
+    """Return a dictionary of all the locations that are not walls.
+    save the type of location, so we can check for incoming/outgoing branches."""
+    path = {}
+    for row, line in enumerate(maze):
+        for col, char in enumerate(line):
+            if char != WALL:
+                path[(row, col)] = char
+    return path
+
+
+def find_ends(maze):
+    """Return the location (row, col) of the start and end tiles,
+    They are the only open tiles in the first and last row respectively"""
+    start_row = 0
+    start_col = None
+    end_row = len(maze) - 1
+    end_col = None
+    for col, char in enumerate(maze[start_row]):
+        if char != WALL:
+            start_col = col
+            break
+    for col, char in enumerate(maze[end_row]):
+        if char != WALL:
+            end_col = col
+            break
+    return (start_row, start_col), (end_row, end_col)
+
+
+def make_junctions(start, end, path):
+    """Return a dictionary of junctions. A Junction is a location with an incoming list
+    and an outgoing list of locations. Either list can be empty. the location is stored
+    as the key; the value is an {'in':[], 'out':[]} dictionary."""
+    junctions = {}
+    for location in path:
+        branches = find_neighbors(location, path)
+        if location == start:
+            junctions[start] = {"in": [], "out": branches}
+            continue
+        if location == end:
+            junctions[end] = {"in": branches, "out": []}
+            continue
+        if len(branches) == 2:
+            continue
+        junctions[location] = {
+            "in": [
+                branch for branch in branches if is_incoming(location, branch, path)
+            ],
+            "out": [
+                branch for branch in branches if not is_incoming(location, branch, path)
+            ],
+        }
+    return junctions
+
+
+def find_neighbors(this, path):
+    """Return a list of adjacent locations that are on the path."""
+    row, col = this
+    neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+    return [n for n in neighbors if n in path]
+
+
+def is_incoming(location, branch, path):
+    """Return True IFF the path location at branch is coming into location"""
+    char = path[branch]
+    row1, col1 = location
+    row2, col2 = branch
+    # above
+    if row2 < row1 and char == DOWN:
+        return True
+    # below
+    if row1 < row2 and char == UP:
+        return True
+    # left
+    if col2 < col1 and char == RIGHT:
+        return True
+    # right
+    if col1 < col2 and char == LEFT:
+        return True
+    return False
+
+
+def make_edges(junctions, path):
+    """Return a list of edges between junctions in the path.
+    an edge is (start_junction, end_junction, length)"""
+    edges = []
+    for junction in junctions:
+        for out in junctions[junction]["out"]:
+            segment = make_segment(junction, out, junctions, path)
+            edges.append((segment[0], segment[-1], len(segment) - 1))
+    return edges
+
+
+def make_segment(junction, next_location, junctions, path):
+    """Return the segment starting at junction, going through next,
+    until the next junction in junctions.  Each segment is a list of all
+    locations along the segment and will always start and stop on a junction
+    """
+    start = junction
+    segment = [start]
+    while next_location not in junctions:
+        segment.append(next_location)
+        start, next_location = find_next(start, next_location, path)
+    segment.append(next_location)
+    return segment
+
+
+def find_next(previous, this, path):
+    """There will always be 2 neighbors of this location, and one will be the previous location.
+    Return the other location, as well as this for the next round"""
+    row, col = this
+    neighbors = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+    for neighbor in neighbors:
+        if neighbor in path and neighbor != previous:
+            return this, neighbor
+    print(f"Find_next failed with {previous}, {this}")
+    return None
+
+
+def make_nodes(junctions):
+    """Return a list where index = node_id, data is location (row,col) of node"""
+    nodes = list(junctions.keys())
+    nodes.sort()
+    return nodes
+
+
+def simplify_edges(edges, nodes):
+    """Replace the location (row, col) in edges with the node id in nodes"""
+    new_edges = []
+    for start, end, length in edges:
+        start_id = nodes.index(start)
+        end_id = nodes.index(end)
+        new_edges.append((start_id, end_id, length))
+    return new_edges
+
+
+def main(filename):
+    """Solve both parts of the puzzle."""
+    _, puzzle = os.path.split(os.path.dirname(__file__))
+    with open(filename, encoding="utf8") as data:
+        lines = data.readlines()
+    print(f"Solving Advent of Code {puzzle} with {filename}")
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
+
+
+if __name__ == "__main__":
+    main(INPUT)
