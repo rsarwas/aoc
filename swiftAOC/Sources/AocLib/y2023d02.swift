f2621041e09a9a23ae6880a1d13b450d72e0531a
Regan Sarwas <rsarwas@gmail.com>
2024-04-19 21:05:23 -0700

    Save templates for unfinished problems

---
+++ swiftAOC/Sources/AocLib/y2023d02.swift
@@ -0,0 +1,19 @@
+struct Problem202302: Problem {
+  var name: String { "2023-02" }
+  func solveWith(data: [String]) -> Solution { Solution202302(data: data) }
+}
+
+struct Solution202302: Solution {
+  let data: [String]
+
+  var part1: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+  var part2: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+}
---
+++ swiftAOC/Sources/AocLib/y2023d03.swift
@@ -0,0 +1,19 @@
+struct Problem202303: Problem {
+  var name: String { "2023-03" }
+  func solveWith(data: [String]) -> Solution { Solution202303(data: data) }
+}
+
+struct Solution202303: Solution {
+  let data: [String]
+
+  var part1: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+  var part2: String {
+    let answer = "Not Implemented"
+    return "\(answer)"
+  }
+
+}
