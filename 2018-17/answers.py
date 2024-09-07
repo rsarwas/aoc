13ae63b7a732cd83c0a9eb0c5add1f0831dab610
Regan Sarwas <rsarwas@gmail.com>
2024-09-06 23:05:27 -0700

    Update 2018-17 to standard template

--- 2018-17/answers.py
+++ 2018-17/answers.py
@@ -1,20 +1,47 @@
+"""A solution to an Advent of Code puzzle."""
+
 # Data Model:
 # ===========
-# lines is a list of "\n" terminated strings from the input file
-#
+# _lines_ is a list of "\n" terminated strings from the input file.
+
+
+import os.path  # to get the directory name of the script (current puzzle year-day)
+
+INPUT = "test.txt"
 
 
 def part1(lines):
-    return -1
+    """Solve part 1 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
 
 
 def part2(lines):
-    return -1
+    """Solve part 2 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
+
+
+def parse(lines):
+    """Convert the lines of text into a useful data model."""
+    data = []
+    for line in lines:
+        line = line.strip()
+        data.append(len(line))
+    return data
+
+
+def main(filename):
+    """Solve both parts of the puzzle."""
+    _, puzzle = os.path.split(os.path.dirname(__file__))
+    with open(filename, encoding="utf8") as data:
+        lines = data.readlines()
+    print(f"Solving Advent of Code {puzzle} with {filename}")
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
 
 
 if __name__ == "__main__":
-    # data = open("input.txt").read() # as one big string
-    lines = open("test.txt").readlines()  # as a list of line strings
-    # lines = open("input.txt").readlines() # as a list of line strings
-    print(f"Part 1: {part1(lines)}")
-    print(f"Part 2: {part2(lines)}")
+    main(INPUT)
