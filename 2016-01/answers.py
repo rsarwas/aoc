d18588669b7a0abd1ebfff44fd68bcad6e76647a
Regan Sarwas <rsarwas@gmail.com>
2024-09-06 22:08:34 -0700

    Solve 2016-01 part1 with python

--- 2016-01/answers.py
+++ 2016-01/answers.py
@@ -9,12 +9,29 @@ import os.path  # to get the directory name of the script (current puzzle year-d
 
 INPUT = "test.txt"
 
+N = 0
+E = 1
+S = 2
+W = 3
+
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
-    return total
+    loc = (0, 0)
+    heading = N
+    for turn, dist in data:
+        heading = update_heading(heading, turn)
+        if heading == N:
+            loc = (loc[0], loc[1] + dist)
+        elif heading == S:
+            loc = (loc[0], loc[1] - dist)
+        elif heading == W:
+            loc = (loc[0] + dist, loc[1])
+        else:
+            loc = (loc[0] - dist, loc[1])
+    man_dist = abs(loc[0]) + abs(loc[1])
+    return man_dist
 
 
 def part2(lines):
@@ -27,10 +44,15 @@ def part2(lines):
 def parse(lines):
     """Convert the lines of text into a useful data model."""
     data = []
-    for line in lines:
-        line = line.strip()
-        data.append(len(line))
+    raw_data = lines[0].strip().split(", ")
+    data = [(item[0], int(item[1:])) for item in raw_data]
     return data
+
+
+def update_heading(heading, turn):
+    if turn == "L":
+        return (heading - 1) % 4
+    return (heading + 1) % 4
 
 
 def main(filename):
