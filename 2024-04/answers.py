e25f5ed2700df8321a3627ffa814293fb6e2a732
Regan Sarwas <rsarwas@gmail.com>
2024-12-03 21:23:19 -0800

    Solve 2024-04 part 1 with Python

--- 2024-04/answers.py
+++ 2024-04/answers.py
@@ -7,13 +7,20 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
+    total = 0
+    num_rows = len(data)
+    num_cols = len(data[0])
+    for r, row in enumerate(data):
+        for c, char in enumerate(row):
+            if char == "X":
+                count = find_xmas(r, num_rows, c, num_cols, data)
+                total += count
     return total
 
 
@@ -29,8 +36,55 @@ def parse(lines):
     data = []
     for line in lines:
         line = line.strip()
-        data.append(len(line))
+        data.append(line)
     return data
+
+
+def find_xmas(r, num_rows, c, num_cols, data):
+    """Return the number (0..8) of occurrences of "XMAS"
+    found in the letters in the grid"""
+    found = 0
+    if r > 2:  # search up
+        if data[r - 1][c] == "M" and data[r - 2][c] == "A" and data[r - 3][c] == "S":
+            found += 1
+    if r > 2 and c < num_cols - 3:  # search up, right
+        if (
+            data[r - 1][c + 1] == "M"
+            and data[r - 2][c + 2] == "A"
+            and data[r - 3][c + 3] == "S"
+        ):
+            found += 1
+    if c < num_cols - 3:  # search right
+        if data[r][c + 1] == "M" and data[r][c + 2] == "A" and data[r][c + 3] == "S":
+            found += 1
+    if r < num_rows - 3 and c < num_cols - 3:  # search down, right
+        if (
+            data[r + 1][c + 1] == "M"
+            and data[r + 2][c + 2] == "A"
+            and data[r + 3][c + 3] == "S"
+        ):
+            found += 1
+    if r < num_rows - 3:  # search down
+        if data[r + 1][c] == "M" and data[r + 2][c] == "A" and data[r + 3][c] == "S":
+            found += 1
+    if r < num_rows - 3 and c > 2:  # search down, left
+        if (
+            data[r + 1][c - 1] == "M"
+            and data[r + 2][c - 2] == "A"
+            and data[r + 3][c - 3] == "S"
+        ):
+            found += 1
+    if c > 2:  # search left
+        if data[r][c - 1] == "M" and data[r][c - 2] == "A" and data[r][c - 3] == "S":
+            found += 1
+    if c > 2 and r > 2:  # search left, up
+        if (
+            data[r - 1][c - 1] == "M"
+            and data[r - 2][c - 2] == "A"
+            and data[r - 3][c - 3] == "S"
+        ):
+            found += 1
+    return found
 
 
 def main(filename):
