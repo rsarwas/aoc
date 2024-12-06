40bfc62815fe120462cce9d9b607fcb993edb852
Regan Sarwas <rsarwas@gmail.com>
2024-12-05 21:49:54 -0800

    Solve 2024-06 part1 with Python

--- 2024-06/answers.py
+++ 2024-06/answers.py
@@ -7,14 +7,19 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
-    data = parse(lines)
-    total = len(data)
-    return total
+    obstructions, guard, size = parse(lines)
+    locations = guard_path(obstructions, guard, size)
+    return len(set(locations))
 
 
 def part2(lines):
@@ -26,11 +31,58 @@ def part2(lines):
 
 def parse(lines):
     """Convert the lines of text into a useful data model."""
-    data = []
-    for line in lines:
+    obstructions = []
+    guard = None
+    size = None
+    row = 0
+    for row, line in enumerate(lines):
         line = line.strip()
-        data.append(len(line))
-    return data
+        for col, char in enumerate(line):
+            if char == "#":
+                obstructions.append((col, row))
+            if char == "^":
+                guard = (col, row, UP)
+        size = len(line)
+    return obstructions, guard, size
+
+
+def guard_path(obstructions, guard, size):
+    """Return a list of all the unigue coordinate pairs that the guard occupies
+    start at guard location/direction. continue until the guard walks off the grid
+    if the guard would an obstruction, turn right"""
+    locations = []
+    while guard[0] >= 0 and guard[0] < size and guard[1] >= 0 and guard[1] < size:
+        locations.append((guard[0], guard[1]))
+        guard = next_location(guard, obstructions)
+    return locations
+
+
+def next_location(guard, obstructions):
+    """return the next location and direction of the guard."""
+    x, y, d = guard
+    nx, ny = x, y
+    if d == UP:
+        ny = y - 1
+    if d == RIGHT:
+        nx = x + 1
+    if d == DOWN:
+        ny = y + 1
+    if d == LEFT:
+        nx = x - 1
+    if (nx, ny) not in obstructions:
+        return (nx, ny, d)
+    else:
+        nx, ny = x, y
+        nd = (d + 1) % 4
+        if nd == UP:
+            ny = y - 1
+        if nd == RIGHT:
+            nx = x + 1
+        if nd == DOWN:
+            ny = y + 1
+        if nd == LEFT:
+            nx = x - 1
+        return (nx, ny, nd)
 
 
 def main(filename):
