80bde178d6ba078f62f5446f84074c04108c21af
Regan Sarwas <rsarwas@gmail.com>
2024-12-09 21:36:41 -0800

    Solve 2024 day 10 part 1 with Python

--- 2024-10/answers.py
+++ 2024-10/answers.py
@@ -7,13 +7,16 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
-    data = parse(lines)
-    total = len(data)
+    map = parse(lines)
+    starts, ends = find_terminals(map)
+    total = 0
+    for start in starts:
+        total += counts_trails(start, ends, map)
     return total
 
 
@@ -29,8 +32,67 @@ def parse(lines):
     data = []
     for line in lines:
         line = line.strip()
-        data.append(len(line))
+        row = [int(c) for c in line]
+        data.append(row)
     return data
+
+
+def find_terminals(map):
+    """Return a list of the coordinates in the map of all the
+    trailheads (value == 0), and all the trailends (value == 9)"""
+    starts = []
+    ends = []
+    for r, row in enumerate(map):
+        for c, height in enumerate(row):
+            if height == 0:
+                starts.append((r, c))
+            if height == 9:
+                ends.append((r, c))
+    return starts, ends
+
+
+def counts_trails(start, ends, map):
+    """Return the number of unique locations in ends that can be reached from start.
+    A trail has to have adjacent locations that increase in height by 1 at each step"""
+    count = 0
+    for end in ends:
+        if within_reach(start, end):
+            if has_trail(0, start, end, map):
+                count += 1
+    return count
+
+
+def within_reach(start, end):
+    """Return True is end is within 9 spaces (taxicab distance) of start"""
+    dr = abs(start[0] - end[0])
+    dc = abs(start[1] - end[1])
+    return dr + dc <= 9
+
+
+def has_trail(start_height, start, end, map):
+    """Return True if we can find a trail from start to end
+    A trail has adjacent squares (up/down/left/right) that increase
+    in height by 1 unit from start to end.
+    This function will be called recursively, so we can find trails of
+    any length, and we do not assume a starting height of 0"""
+    if start_height == 9:
+        return start == end
+    else:
+        r, c = start
+        height = start_height + 1
+        if r - 1 >= 0 and map[r - 1][c] == height:
+            if has_trail(height, (r - 1, c), end, map):
+                return True
+        if r + 1 < len(map) and map[r + 1][c] == height:
+            if has_trail(height, (r + 1, c), end, map):
+                return True
+        if c - 1 >= 0 and map[r][c - 1] == height:
+            if has_trail(height, (r, c - 1), end, map):
+                return True
+        if c + 1 < len(map[r]) and map[r][c + 1] == height:
+            if has_trail(height, (r, c + 1), end, map):
+                return True
+    return False
 
 
 def main(filename):
