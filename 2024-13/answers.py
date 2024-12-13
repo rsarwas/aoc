f926472197752ec556323d763c83a89af079fed6
Regan Sarwas <rsarwas@gmail.com>
2024-12-12 22:58:01 -0800

    Solve 2024 day 13 part1 with Python

--- 2024-13/answers.py
+++ 2024-13/answers.py
@@ -7,14 +7,19 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
-    return total
+    total = 0
+    n = 1
+    for machine in data:
+        solution = solve(machine)
+        if is_valid(solution):
+            total += cost(solution)
+    return int(round(total, 5))
 
 
 def part2(lines):
@@ -27,10 +32,48 @@ def part2(lines):
 def parse(lines):
     """Convert the lines of text into a useful data model."""
     data = []
-    for line in lines:
-        line = line.strip()
-        data.append(len(line))
+    i = 0
+    while i + 3 <= len(lines):
+        button_a = lines[i + 0].strip().replace("Button A: X+", "").split(", Y+")
+        button_a = [int(x) for x in button_a]
+        button_b = lines[i + 1].strip().replace("Button B: X+", "").split(", Y+")
+        button_b = [int(x) for x in button_b]
+        prize = lines[i + 2].strip().replace("Prize: X=", "").split(", Y=")
+        prize = [int(x) for x in prize]
+        machine = (button_a, button_b, prize)
+        data.append(machine)
+        i += 4
     return data
+
+
+def solve(machine):
+    """Solve a simple system of linear equations
+    a1*x + b1*y = c1
+    a2*x + b2*y == c2
+
+    where x is the number of button pushes for A and y is for B.
+    a1,b1,c1 are distances along the X axis  and a2,b2,c2 are along y."""
+    button_a, button_b, prize = machine
+    a1, a2 = button_a
+    b1, b2 = button_b
+    c1, c2 = prize
+    y = (c1 - c2 * a1 / a2) / (b1 - b2 * a1 / a2)
+    x = (c1 - b1 * y) / a1
+    return (x, y)
+
+
+def is_valid(solution):
+    """Checks if the solution is valid. i.e. a positive number of integral button pushes"""
+    a, b = solution
+    # round the numbers to remove floating point epsilon
+    a, b = round(a, 5), round(b, 5)
+    return a > 0 and b > 0 and int(a) == a and int(b) == b
+
+
+def cost(solution):
+    """return the cost of the solution"""
+    a, b = solution
+    return a * 3 + b
 
 
 def main(filename):
