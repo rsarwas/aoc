75ff112fd24f16c06a388f12617f3a67444d3ad1
Regan Sarwas <rsarwas@gmail.com>
2024-12-22 23:48:06 -0800

    Solve 2024 day 23 part1 with Python

--- 2024-23/answers.py
+++ 2024-23/answers.py
@@ -7,13 +7,21 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
+    # print(data)
+    group3 = groups_of_three(data)
+    # print(group3)
+    computers = [c for c in data.keys() if c.startswith("t")]
+    # print(computers)
+    total = 0
+    for c1, c2, c3 in group3:
+        if c1 in computers or c2 in computers or c3 in computers:
+            total += 1
     return total
 
 
@@ -26,11 +34,30 @@ def part2(lines):
 
 def parse(lines):
     """Convert the lines of text into a useful data model."""
-    data = []
+    data = {}
     for line in lines:
-        line = line.strip()
-        data.append(len(line))
+        computer1, computer2 = line.strip().split("-")
+        if computer1 not in data:
+            data[computer1] = []
+        if computer2 not in data:
+            data[computer2] = []
+        data[computer1].append(computer2)
+        data[computer2].append(computer1)
     return data
+
+
+def groups_of_three(data):
+    """Return a set of the 3-pules of interconnected keys in data"""
+    groups = set()
+    for computer1 in data:
+        for index, computer2 in enumerate(data[computer1][:-1]):
+            for computer3 in data[computer1][index + 1 :]:
+                if computer3 in data[computer2]:
+                    # add the three to a list/set, but make sure it is unique
+                    computers = [computer1, computer2, computer3]
+                    computers.sort()
+                    groups.add((computers[0], computers[1], computers[2]))
+    return groups
 
 
 def main(filename):
