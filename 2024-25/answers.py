2c39b57f9060f93ad6cb8373f61f2188883e60df
Regan Sarwas <rsarwas@gmail.com>
2024-12-24 21:35:08 -0800

    Solve 2024 day 25 parts 1 and 2 with Python

--- 2024-25/answers.py
+++ 2024-25/answers.py
@@ -7,30 +7,94 @@
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
+    keys, locks = split(data)
+    keys = encode_keys(keys)
+    locks = encode_locks(locks)
+    # print(keys)
+    # print(locks)
+    total = 0
+    for key in keys:
+        for lock in locks:
+            if fit(key, lock):
+                total += 1
     return total
 
 
 def part2(lines):
     """Solve part 2 of the problem."""
-    data = parse(lines)
-    total = len(data)
-    return total
+    return "Merry Christmas!"
 
 
 def parse(lines):
     """Convert the lines of text into a useful data model."""
     data = []
+    n = 0
     for line in lines:
         line = line.strip()
-        data.append(len(line))
+        if not line:
+            continue
+        key_id = n // 7
+        row = n % 7
+        if row == 0:
+            key = []
+            data.append(key)
+        key.append(line)
+        n += 1
     return data
+
+
+def split(data):
+    """Split the data into keys and locks"""
+    keys = []
+    locks = []
+    for item in data:
+        if item[0] == "#####":
+            locks.append(item)
+        else:
+            keys.append(item)
+    return keys, locks
+
+
+def encode_keys(items):
+    """Encode the height of the teeth on the key"""
+    keys = []
+    for item in items:
+        key = [0, 0, 0, 0, 0]
+        for col in range(5):
+            for row in range(7):
+                if item[row][col] == "#":
+                    key[col] = row
+                    break
+        keys.append(key)
+    return keys
+
+
+def encode_locks(items):
+    """Encode the height of the teeth on the key"""
+    locks = []
+    for item in items:
+        lock = [0, 0, 0, 0, 0]
+        for col in range(5):
+            for row in range(7):
+                if item[row][col] == ".":
+                    lock[col] = row
+                    break
+        locks.append(lock)
+    return locks
+
+
+def fit(key, lock):
+    """Test if a key fits in a lock"""
+    for index in range(5):
+        if lock[index] > key[index]:
+            return False
+    return True
 
 
 def main(filename):
