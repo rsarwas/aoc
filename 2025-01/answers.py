6e820abc30668886e35f5a4daba8ff68f183628d
Regan Sarwas <rsarwas@gmail.com>
2025-12-01 22:58:09 -0800

    Solve 2025-01b

--- 2025-01/answers.py
+++ 2025-01/answers.py
@@ -26,7 +26,36 @@ def part1(lines):
 def part2(lines):
     """Solve part 2 of the problem."""
     data = parse(lines)
-    total = len(data)
+    total = 0
+    value = 50
+    for item in data:
+        # print(value, item)
+        old_value = value
+        value += item
+        if item < 0:
+            # Moving left to smaller numbers, usually if if the nev value is negative, then we have crossed
+            # zero at least once, however this is not true if we start at zero.
+            if old_value == 0:
+                # item -1 to -99 => 0 (item//-100)
+                # item -100 to -199 => 1 (item//-100)
+                total += value // -100
+            else:
+                # if value > 0 then add nothing to total
+                if value <= 0:
+                    # x = original value; item = move in ();  new value = old_value - move
+                    # if item in 1..x-1, then x + item is positive and zero count is 0;  i.e count = 0 if value + item > 0
+                    # x-(x..100+x-1) = 1;  -99..0; count = 1 == (value + item)//-100 + 1
+                    # x-(100+x..200+x-1) = 2: -199..-100;; 2 == (value + item)//-100 + 1
+                    # ...  count of zeros = 2 == (value + item)//-100 + 1
+                    total += 1 + value // -100
+        else:  # rotate CW (positive item)
+            # value = old_value + item (move count to right(+));  zero_count
+            # value < 99 => zero_count = 0
+            # value in 100..199 => zero_count = 1
+            # value in 200..299 => zero_count = 2
+            # ...  zero_count = value//100
+            total += value // 100
+        value %= 100
     return total
 
 
