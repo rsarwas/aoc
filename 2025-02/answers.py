d56ffad3b3c793629f47049c272186961efb1288
Regan Sarwas <rsarwas@gmail.com>
2025-12-01 22:16:11 -0800

    Solve 2025-02b

--- 2025-02/answers.py
+++ 2025-02/answers.py
@@ -26,7 +26,13 @@ def part1(lines):
 def part2(lines):
     """Solve part 2 of the problem."""
     data = parse(lines)
-    total = len(data)
+    total = 0
+    for n1, n2 in data:
+        for n in range(n1, n2 + 1):
+            if invalid2(n):
+                if INPUT == "test.txt":
+                    print(n1, n2, n)
+                total += n
     return total
 
 
@@ -48,6 +54,24 @@ def invalid(n):
     l2 = (l + 1) // 2
     s = str(n)
     return s[:l2] == s[l2:]
+
+
+def invalid2(n):
+    """Returns true if n is invalid; i.e. has a repeating pattern, e.g 121212"""
+    s = str(n)
+    for n in range(1, len(s) // 2 + 1):
+        if invalid_n(s, n):
+            return True
+    return False
+
+
+def invalid_n(s, n):
+    """Return true if s is a sequence of the first n chars repeated"""
+    start = s[:n]
+    for x in range(n, len(s), n):
+        if start != s[x : x + n]:
+            return False
+    return True
 
 
 def main(filename):
