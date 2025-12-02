297fb8329a9ead028aaf0e778ad9908b4d60df9a
Regan Sarwas <rsarwas@gmail.com>
2025-12-01 21:48:04 -0800

    Solve 2025-02a

--- 2025-02/answers.py
+++ 2025-02/answers.py
@@ -6,14 +6,20 @@
 
 
 import os.path  # to get the directory name of the script (current puzzle year-day)
+import math  # for log10, to determine number of digits in number
 
-INPUT = "test.txt"
+INPUT = "input.txt"
 
 
 def part1(lines):
     """Solve part 1 of the problem."""
     data = parse(lines)
-    total = len(data)
+    total = 0
+    for n1, n2 in data:
+        for n in range(n1, n2 + 1):
+            if invalid(n):
+                # print(n1, n2, n)
+                total += n
     return total
 
 
@@ -27,10 +33,21 @@ def part2(lines):
 def parse(lines):
     """Convert the lines of text into a useful data model."""
     data = []
-    for line in lines:
-        line = line.strip()
-        data.append(len(line))
+    for rng in lines[0].strip().split(","):
+        x, y = rng.split("-")
+        data.append((int(x), int(y)))
     return data
+
+
+def invalid(n):
+    """Returns true if n is invalid; i.e. first half matches second half; e.g. 9797"""
+    l = int(math.log10(n))
+    if l % 2 == 0:
+        # l is an even number, so there is an odd number of digits
+        return False
+    l2 = (l + 1) // 2
+    s = str(n)
+    return s[:l2] == s[l2:]
 
 
 def main(filename):
