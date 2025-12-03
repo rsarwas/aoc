e62c19838cdd4ca560e7db446988e4abb6c127dd
Regan Sarwas <rsarwas@gmail.com>
2025-12-02 21:50:13 -0800

    Add last two python templates for 2025

---
+++ 2025-11/answers.py
@@ -0,0 +1,47 @@
+"""A solution to an Advent of Code puzzle."""
+
+# Data Model:
+# ===========
+# _lines_ is a list of "\n" terminated strings from the input file.
+
+
+import os.path  # to get the directory name of the script (current puzzle year-day)
+
+INPUT = "test.txt"
+
+
+def part1(lines):
+    """Solve part 1 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
+
+
+def part2(lines):
+    """Solve part 2 of the problem."""
+    data = parse(lines)
+    total = len(data)
+    return total
+
+
+def parse(lines):
+    """Convert the lines of text into a useful data model."""
+    data = []
+    for line in lines:
+        line = line.strip()
+        data.append(len(line))
+    return data
+
+
+def main(filename):
+    """Solve both parts of the puzzle."""
+    _, puzzle = os.path.split(os.path.dirname(__file__))
+    with open(filename, encoding="utf8") as data:
+        lines = data.readlines()
+    print(f"Solving Advent of Code {puzzle} with {filename}")
+    print(f"Part 1: {part1(lines)}")
+    print(f"Part 2: {part2(lines)}")
+
+
+if __name__ == "__main__":
+    main(INPUT)
